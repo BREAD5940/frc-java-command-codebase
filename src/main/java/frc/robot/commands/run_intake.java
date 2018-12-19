14b1b186e915b0048c8f14f3639dbb175ad47c0d
Matthew Morley <matthew.morley.ca@gmail.com>
2018-12-18 22:05:37 -0800

    Create run_intake.java

---
+++ src/main/java/frc/robot/commands/run_intake.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+
+
+/**
+ * Shifter command to shift to high gear
+ */
+public class run_intake extends Command {
+  public run_intake() {
+    // Use requires() here to declare subsystem dependencies
+    // requires(Robot.intake); // TODO do I need to reserve the inktake? No, I don't think so. Reserve it instead for the intake auto action
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+    Robot.intake.setSpeed(Robot.m_oi.getIntakeAxis() - Robot.m_oi.getOuttakeAxis());
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
