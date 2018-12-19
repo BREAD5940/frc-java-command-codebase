61719a352701fb2c2172ef4adfa1edc8209a36b3
mcm001 <matthew.morley.ca@gmail.com>
2018-12-19 15:45:47 -0800

    yo what the heck pheumatics stopped working

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -42,14 +42,14 @@ public class Robot extends TimedRobot {
 
   public static double elevator_setpoint = 0;
   public static double wrist_setpoint = 0;
-  private static DoubleSolenoid shifterDoubleSolenoid = new DoubleSolenoid(9, 7, 3);
-  private static DoubleSolenoid intakeDoubleSolenoid = new DoubleSolenoid(9, 0, 6);
+  public static DoubleSolenoid shifterDoubleSolenoid = new DoubleSolenoid(9, 7, 3);
+  public static DoubleSolenoid intakeDoubleSolenoid = new DoubleSolenoid(9, 0, 6);
 
   Command m_autonomousCommand;
   SendableChooser<Command> m_chooser = new SendableChooser<>();
 
-  public static void drivetrain_shift_high(){ shifterDoubleSolenoid.set(DoubleSolenoid.Value.kForward); }
-  public static void drivetrain_shift_low(){ shifterDoubleSolenoid.set(DoubleSolenoid.Value.kReverse); }
+  public static void drivetrain_solenoid_shift_high(){ shifterDoubleSolenoid.set(DoubleSolenoid.Value.kForward); }
+  public static void drivetrain_solenoid_shift_low(){ shifterDoubleSolenoid.set(DoubleSolenoid.Value.kReverse); }
   public static void intake_close(){ intakeDoubleSolenoid.set(DoubleSolenoid.Value.kForward); }
   public static void intake_open(){ intakeDoubleSolenoid.set(DoubleSolenoid.Value.kReverse); }
 
@@ -149,6 +149,9 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.start();
     }
+
+ 
+
   }
 
   /**
@@ -170,10 +173,6 @@ public class Robot extends TimedRobot {
       m_autonomousCommand.cancel();
     }
 
-    // final arcade_drive arcade = new arcade_drive();
-
-  
-
   }
 
   /**
@@ -184,6 +183,8 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     Scheduler.getInstance().run();
     
+    drivetrain_solenoid_shift_low();
+
     /**
      * Update the arcade drivetrain method
      */
@@ -193,7 +194,6 @@ public class Robot extends TimedRobot {
     // drivetrain.m_right_talon.set(ControlMode.PercentOutput, 0.25);
 
     // drivetrain_shift_low();
-    drivetrain_shift_high();
     
     /**
      * Update the elevator PID method
