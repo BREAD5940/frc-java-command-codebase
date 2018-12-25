42a56363651a2b5262daa20a33ff50538d94234d
mcm001 <matthew.morley.ca@gmail.com>
2018-12-25 09:45:51 -0800

    drivetrain: fix indentation, move init, add setSpeeds

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -25,111 +25,107 @@ public class DriveTrain extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-    // Robot Robot = new Robot();
-
-    public TalonSRX m_left_talon = new TalonSRX(RobotConfig.m_left_talon_port);
-    public TalonSRX s_left_talon = new TalonSRX(RobotConfig.s_left_talon_port);
-    public TalonSRX m_right_talon = new TalonSRX(RobotConfig.m_right_talon_port);
-    public TalonSRX s_right_talon = new TalonSRX(RobotConfig.s_right_talon_port);
-    public String current_gear;
+  public TalonSRX m_left_talon = new TalonSRX(RobotConfig.m_left_talon_port);
+  public TalonSRX s_left_talon = new TalonSRX(RobotConfig.s_left_talon_port);
+  public TalonSRX m_right_talon = new TalonSRX(RobotConfig.m_right_talon_port);
+  public TalonSRX s_right_talon = new TalonSRX(RobotConfig.s_right_talon_port);
+  public String current_gear;
 
 
-    // PIDController left_position_pid_controller_HIGH_GEAR = new PIDController(RobotConfig.m_left_position_kp_high, RobotConfig.m_left_position_ki_high, RobotConfig.m_left_position_kd_high, RobotConfig.m_left_position_kf_high, source, output) // TODO Implament position PID controllers for left and right drivetrain sides
+  // PIDController left_position_pid_controller_HIGH_GEAR = new PIDController(RobotConfig.m_left_position_kp_high, RobotConfig.m_left_position_ki_high, RobotConfig.m_left_position_kd_high, RobotConfig.m_left_position_kf_high, source, output) // TODO Implament position PID controllers for left and right drivetrain sides
 
 
 
-    // Robot robot = new Robot(); 
-    
-
-    public void init() {
-      m_left_talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,0,30);
-      m_right_talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,0,30);
-      s_left_talon.set(ControlMode.Follower, m_left_talon.getDeviceID());
-      s_right_talon.set(ControlMode.Follower, m_right_talon.getDeviceID());
-      m_right_talon.configSensorTerm(SensorTerm.Diff0, FeedbackDevice.QuadEncoder, 30); // Quadrature Encoder of current Talon
-      m_left_talon.configSensorTerm(SensorTerm.Diff0, FeedbackDevice.QuadEncoder, 30); // Quadrature Encoder of current Talon
-
-      m_left_talon.configPeakOutputForward(+1.0, 30);
-      m_left_talon.configPeakOutputReverse(-1.0, 30);
-      m_right_talon.configPeakOutputForward(+1.0, 30);
-      m_right_talon.configPeakOutputReverse(-1.0, 30);
-
-
-    	/* 1ms per loop.  PID loop can be slowed down if need be.
-      * For example,
-      * - if sensor updates are too slow
-      * - sensor deltas are very small per update, so derivative error never gets large enough to be useful.
-      * - sensor movement is very slow causing the derivative error to be near zero.
-      */
-      int closedLoopTimeMs = 1;
-      // TODO can I get rid of this code, or is it disabling something important?
-      m_right_talon.configSetParameter(ParamEnum.eSampleVelocityPeriod, closedLoopTimeMs, 0x00, 0,30);
-      m_right_talon.configSetParameter(ParamEnum.eSampleVelocityPeriod, closedLoopTimeMs, 0x00, 1, 30);
-      
-      m_left_talon.setInverted(true);
-      s_left_talon.setInverted(true);
-
-      m_left_talon.setSelectedSensorPosition(0, 0, 30);
-      m_right_talon.setSelectedSensorPosition(0, 0, 30);
-
-      // public String gear_state;
-
-      // Robot.shifter.set(DoubleSolenoid.Value.kForward);
-      // Robot.shifter.set(DoubleSolenoid.Value.kReverse);
-
-      setHighGear();
-
-      // if ( RobotConfig.drivetrain_starting_gear == "low" ) {
-      //   new drivetrain_shift_low();
-      // }
-      // if ( RobotConfig.drivetrain_starting_gear == "high" ) {
-      //   new drivetrain_shift_high();
-      // }
-
-    }
-    
-
-
-    public void setHighGear() {
-      this.m_left_talon.config_kP(0, RobotConfig.m_left_velocity_kp_high, 30);
-      this.m_left_talon.config_kI(0, RobotConfig.m_left_velocity_ki_high, 30);
-      this.m_left_talon.config_kD(0, RobotConfig.m_left_velocity_kd_high, 30);
-      this.m_left_talon.config_kF(0, RobotConfig.m_left_velocity_kf_high, 30);
-      this.m_left_talon.config_IntegralZone(0, RobotConfig.m_left_velocity_izone_high, 30);
-      // this.m_left_talon.configMaxIntegralAccumulator(0, RobotConfig.m_left_velocity_max_integral_high, 0);
+  // Robot robot = new Robot(); 
   
-      this.m_right_talon.config_kP(0, RobotConfig.m_right_velocity_kp_high, 30);
-      this.m_right_talon.config_kI(0, RobotConfig.m_right_velocity_ki_high, 30);
-      this.m_right_talon.config_kD(0, RobotConfig.m_right_velocity_kd_high, 30);
-      this.m_right_talon.config_kF(0, RobotConfig.m_right_velocity_kf_high, 30);
-      this.m_right_talon.config_IntegralZone(0, RobotConfig.m_right_velocity_izone_high, 30);
-      // this.m_right_talon.configMaxIntegralAccumulator(0, RobotConfig.m_right_velocity_max_integral_high, 0);
-      
-      // Trigger solenoids
-      Robot.drivetrain_shift_high();
-      current_gear = "high";
-    }
 
-    public void setLowGear() {
-      this.m_left_talon.config_kP(0, RobotConfig.m_left_velocity_kp_low, 0);
-      this.m_left_talon.config_kI(0, RobotConfig.m_left_velocity_ki_low, 0);
-      this.m_left_talon.config_kD(0, RobotConfig.m_left_velocity_kd_low, 0);
-      this.m_left_talon.config_kF(0, RobotConfig.m_left_velocity_kf_low, 0);
-      this.m_left_talon.config_IntegralZone(0, RobotConfig.m_left_velocity_izone_low, 0);
-      // this.m_left_talon.configMaxIntegralAccumulator(0, RobotConfig.m_left_velocity_max_integral_low, 0);
-      
-      this.m_right_talon.config_kP(0, RobotConfig.m_right_velocity_kp_low, 0);
-      this.m_right_talon.config_kI(0, RobotConfig.m_right_velocity_ki_low, 0);
-      this.m_right_talon.config_kD(0, RobotConfig.m_right_velocity_kd_low, 0);
-      this.m_right_talon.config_kF(0, RobotConfig.m_right_velocity_kf_low, 0);
-      this.m_right_talon.config_IntegralZone(0, RobotConfig.m_right_velocity_izone_low, 0);
-      // this.m_right_talon.configMaxIntegralAccumulator(0, RobotConfig.m_right_velocity_max_integral_low, 0);
+  public void init() {
+    m_left_talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,0,30);
+    m_right_talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,0,30);
+    s_left_talon.set(ControlMode.Follower, m_left_talon.getDeviceID());
+    s_right_talon.set(ControlMode.Follower, m_right_talon.getDeviceID());
+    m_right_talon.configSensorTerm(SensorTerm.Diff0, FeedbackDevice.QuadEncoder, 30); // Quadrature Encoder of current Talon
+    m_left_talon.configSensorTerm(SensorTerm.Diff0, FeedbackDevice.QuadEncoder, 30); // Quadrature Encoder of current Talon
+
+    m_left_talon.configPeakOutputForward(+1.0, 30);
+    m_left_talon.configPeakOutputReverse(-1.0, 30);
+    m_right_talon.configPeakOutputForward(+1.0, 30);
+    m_right_talon.configPeakOutputReverse(-1.0, 30);
+
+
+    /* 1ms per loop.  PID loop can be slowed down if need be.
+    * For example,
+    * - if sensor updates are too slow
+    * - sensor deltas are very small per update, so derivative error never gets large enough to be useful.
+    * - sensor movement is very slow causing the derivative error to be near zero.
+    */
+    int closedLoopTimeMs = 1;
+    // TODO can I get rid of this code, or is it disabling something important?
+    m_right_talon.configSetParameter(ParamEnum.eSampleVelocityPeriod, closedLoopTimeMs, 0x00, 0,30);
+    m_right_talon.configSetParameter(ParamEnum.eSampleVelocityPeriod, closedLoopTimeMs, 0x00, 1, 30);
+    
+    m_left_talon.setInverted(true);
+    s_left_talon.setInverted(true);
+
+    m_left_talon.setSelectedSensorPosition(0, 0, 30);
+    m_right_talon.setSelectedSensorPosition(0, 0, 30);
+
+    // public String gear_state;
+
+    // Robot.shifter.set(DoubleSolenoid.Value.kForward);
+    // Robot.shifter.set(DoubleSolenoid.Value.kReverse);
+
+    setHighGear();
+
+    // if ( RobotConfig.drivetrain_starting_gear == "low" ) {
+    //   new drivetrain_shift_low();
+    // }
+    // if ( RobotConfig.drivetrain_starting_gear == "high" ) {
+    //   new drivetrain_shift_high();
+    // }
+
+  }
   
-      // Trigger solenoids
-      Robot.drivetrain_shift_low();
+  public void setHighGear() {
+    this.m_left_talon.config_kP(0, RobotConfig.m_left_velocity_kp_high, 30);
+    this.m_left_talon.config_kI(0, RobotConfig.m_left_velocity_ki_high, 30);
+    this.m_left_talon.config_kD(0, RobotConfig.m_left_velocity_kd_high, 30);
+    this.m_left_talon.config_kF(0, RobotConfig.m_left_velocity_kf_high, 30);
+    this.m_left_talon.config_IntegralZone(0, RobotConfig.m_left_velocity_izone_high, 30);
+    // this.m_left_talon.configMaxIntegralAccumulator(0, RobotConfig.m_left_velocity_max_integral_high, 0);
 
-      current_gear = "low";
-    }
+    this.m_right_talon.config_kP(0, RobotConfig.m_right_velocity_kp_high, 30);
+    this.m_right_talon.config_kI(0, RobotConfig.m_right_velocity_ki_high, 30);
+    this.m_right_talon.config_kD(0, RobotConfig.m_right_velocity_kd_high, 30);
+    this.m_right_talon.config_kF(0, RobotConfig.m_right_velocity_kf_high, 30);
+    this.m_right_talon.config_IntegralZone(0, RobotConfig.m_right_velocity_izone_high, 30);
+    // this.m_right_talon.configMaxIntegralAccumulator(0, RobotConfig.m_right_velocity_max_integral_high, 0);
+    
+    // Trigger solenoids
+    Robot.drivetrain_shift_high();
+    current_gear = "high";
+  }
+
+  public void setLowGear() {
+    this.m_left_talon.config_kP(0, RobotConfig.m_left_velocity_kp_low, 0);
+    this.m_left_talon.config_kI(0, RobotConfig.m_left_velocity_ki_low, 0);
+    this.m_left_talon.config_kD(0, RobotConfig.m_left_velocity_kd_low, 0);
+    this.m_left_talon.config_kF(0, RobotConfig.m_left_velocity_kf_low, 0);
+    this.m_left_talon.config_IntegralZone(0, RobotConfig.m_left_velocity_izone_low, 0);
+    // this.m_left_talon.configMaxIntegralAccumulator(0, RobotConfig.m_left_velocity_max_integral_low, 0);
+    
+    this.m_right_talon.config_kP(0, RobotConfig.m_right_velocity_kp_low, 0);
+    this.m_right_talon.config_kI(0, RobotConfig.m_right_velocity_ki_low, 0);
+    this.m_right_talon.config_kD(0, RobotConfig.m_right_velocity_kd_low, 0);
+    this.m_right_talon.config_kF(0, RobotConfig.m_right_velocity_kf_low, 0);
+    this.m_right_talon.config_IntegralZone(0, RobotConfig.m_right_velocity_izone_low, 0);
+    // this.m_right_talon.configMaxIntegralAccumulator(0, RobotConfig.m_right_velocity_max_integral_low, 0);
+
+    // Trigger solenoids
+    Robot.drivetrain_shift_low();
+
+    current_gear = "low";
+  }
 
   public double getLeftDistance() {return EncoderLib.rawToDistance(this.m_left_talon.getSelectedSensorPosition(0), 
     RobotConfig.POSITION_PULSES_PER_ROTATION, RobotConfig.left_wheel_effective_diameter / 12); }
@@ -139,6 +135,17 @@ public class DriveTrain extends Subsystem {
     RobotConfig.POSITION_PULSES_PER_ROTATION, RobotConfig.left_wheel_effective_diameter);}
   public double getRightVelocity() {return EncoderLib.rawToDistance(this.m_right_talon.getSelectedSensorVelocity(0) * 10, 
     RobotConfig.POSITION_PULSES_PER_ROTATION, RobotConfig.right_wheel_effective_diameter);}
+
+  /**
+   * Set the drivetrain target speed as two doubles. For
+   * all ye lazy programmers
+   * @param speed_left_raw raw left speed
+   * @param speed_right_raw raw right speed
+   */
+  public void setSpeeds(double speed_left_raw, double speed_right_raw) {
+    setLeftSpeedRaw(speed_left_raw);
+    setRightSpeedRaw(speed_right_raw);
+  }
 
   /**
    * Set the target left speed. Units are in raw units.
