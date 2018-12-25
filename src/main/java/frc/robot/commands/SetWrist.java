a4810150d049a938dadad9906524444308d75a4a
mcm001 <matthew.morley.ca@gmail.com>
2018-12-24 21:00:09 -0800

    SetWrist: add multiple constructors

--- src/main/java/frc/robot/commands/SetWrist.java
+++ src/main/java/frc/robot/commands/SetWrist.java
@@ -8,12 +8,30 @@ import frc.robot.lib.EncoderLib;
 public class SetWrist extends Command {
     boolean isInstant;
     double target_angle;
-    boolean am_i_done = false;
     double position_tolerence = EncoderLib.degreesToRaw(RobotConfig.wrist_position_tolerence,RobotConfig.POSITION_PULSES_PER_ROTATION);
     double velocity_tolerence = EncoderLib.degreesToRaw(RobotConfig.wrist_velocity_tolerence, RobotConfig.POSITION_PULSES_PER_ROTATION);   // TODO verify this behavior, maybe omit for now?
 
+    /**
+     * This constructor takes a target angle in degrees and the boolean isInstant.
+     * If you want thsi command to return instantly, use the one argument constructor 
+     * unless you want to be verbose. If isInstant is true, this command will only
+     * return if both position and angular velocity are within thresholds.
+     * @param target_angle in degrees from zero
+     * @param isInstant boolean
+     */
     public SetWrist(double target_angle, boolean isInstant) {
         this.isInstant = isInstant;
+        this.target_angle = target_angle;
+        requires(Robot.wrist); // reserve the wrist subsystem
+    }
+
+    /**
+     * Boring version of the set wrist constructor. Defaults to instant return without waiting for angle.
+     * If you want to wait for the wrist to reach the target angle, set the second argument to false.
+     * @param target_angle in degrees from zero
+     */
+    public SetWrist(double target_angle) {
+        this.isInstant = true; // TODO review if the default behavior of the wrist (to not wait for angle before returning) is desirable
         this.target_angle = target_angle;
         requires(Robot.wrist); // reserve the wrist subsystem
     }
@@ -22,7 +40,7 @@ public class SetWrist extends Command {
 
     // Called just before this Command runs the first time
     @Override
-    protected void initialize() { // TODO verify that I can just set the setpoint and walk away
+    protected void initialize() {
         Robot.wrist.setAngle(target_angle);
     }
 
