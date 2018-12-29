a11fa30038c130c1c557335e5ea21b903d67e1fd
unduli-attacked <45951442+unduli-attacked@users.noreply.github.com>
2018-12-28 20:50:12 -0800

    begin working on command groups

---
+++ src/main/java/frc/robot/auto/actions/auto_DRIVE_RIGHT.java
@@ -0,0 +1,15 @@
+package frc.robot.auto.actions;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class auto_DRIVE_RIGHT extends CommandGroup{
+
+    /**
+     * forms a sequential command group of right turns and straight stretches of driving
+     * @param turns
+     * @param stretches
+     */
+    public auto_DRIVE_RIGHT(String turns, Integer... stretches){
+
+    }
+}
