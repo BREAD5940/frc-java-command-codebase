776a8842987201e694f2ef1bdc17ae9cad15f45f
mcm001 <matthew.morley.ca@gmail.com>
2019-01-03 17:46:45 -0800

    add unit tests

---
+++ src/test/java/OdometryTest.java
@@ -0,0 +1,17 @@
+import frc.robot.lib.Odometry;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class OdometryTest {
+
+    @Test
+    public void StraightMoveForwardTest() {
+        boolean x = false;
+        boolean y = true;
+        assertTrue(x, "your test failed ur badg");
+    }
+}
