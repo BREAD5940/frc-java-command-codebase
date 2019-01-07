d43711e7d90472cd2211f8b9b6287b4fc242328b
Matthew Morley <matthew.morley.ca@gmail.com>
2019-01-06 22:10:25 -0800

    Update PrepareIntake.java

--- src/main/java/frc/robot/commands/groups/PrepareIntake.java
+++ src/main/java/frc/robot/commands/groups/PrepareIntake.java
@@ -3,10 +3,13 @@ package frc.robot.commands.groups;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
 import frc.robot.auto.actions.auto_Elevator;
+import frc.robot.commands.SetWrist;
+import frc.robot.subsystems.Elevator.ElevatorHeight;
 
 public class PrepareIntake extends CommandGroup {
     public PrepareIntake(ElevatorHeight height) {
         Robot.intake.setSpeed(0);
-        addParallel( new auto_Elevator(demand, isInstant) )
+        addParallel( new auto_Elevator(Robot.elevator.getHeightEnumValue(ElevatorHeight.CARGO_SHIP_HATCH), true) );
+        addParallel( new SetWrist(90) );
     }
 }
