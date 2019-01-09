a274e51bd7c26c79f099f4311d292fb9d5da7f1c
Cole Gannon <Arcanehood@gmail.com>
2019-01-09 02:02:43 -0800

    No functionality changes. Probably

--- src/main/java/frc/robot/auto/actions/TurnInPlace.java
+++ src/main/java/frc/robot/auto/actions/TurnInPlace.java
@@ -37,16 +37,16 @@ public class TurnInPlace extends Command {
   //   RobotConfig.auto.TurnInPlace.max_integral);
   
   // public TerriblePID(double kp, double ki, double minOutput, double maxOutput, double integralZone, double maxIntegralAccum, double kf, FeedForwardMode feedforwardmode, FeedForwardBehavior feedforwardbehavior, double unitsPerQuarterWave) {
-  TerriblePID turnPID = new TerriblePID(RobotConfig.auto.auto_turnInPlace.kp, RobotConfig.auto.auto_turnInPlace.ki, 
-    RobotConfig.auto.auto_turnInPlace.min_turn_speed, 
-    RobotConfig.auto.auto_turnInPlace.max_turn_speed, 
-    RobotConfig.auto.auto_turnInPlace.integral_zone, 
-    RobotConfig.auto.auto_turnInPlace.max_integral,
-    RobotConfig.auto.auto_turnInPlace.kf,
+  TerriblePID turnPID = new TerriblePID(RobotConfig.auto.turnInPlace.kp, RobotConfig.auto.turnInPlace.ki, 
+    RobotConfig.auto.turnInPlace.min_turn_speed, 
+    RobotConfig.auto.turnInPlace.max_turn_speed, 
+    RobotConfig.auto.turnInPlace.integral_zone, 
+    RobotConfig.auto.turnInPlace.max_integral,
+    RobotConfig.auto.turnInPlace.kf,
     FeedForwardMode.LINEAR, 
     FeedForwardBehavior.NORMAL,
     1
-    );
+  );
 
 
 
