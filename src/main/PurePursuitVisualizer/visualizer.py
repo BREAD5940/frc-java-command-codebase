b621273f7a44e631e7b85606a5dbd2e400ace466
DMC2 <dmc2@127.0.0.1>
2019-01-10 12:33:04 -0800

    More python

--- src/main/PurePursuitVisualizer/visualizer.py
+++ src/main/PurePursuitVisualizer/visualizer.py
@@ -18,15 +18,28 @@ fieldsize = [54, 27] # in feet
 fieldSizeInPixels = [ (1372 - 217), (615 - 40) ]
 imagesize = [1592, 676]
 
-def plotFromFieldCoordinates ( fieldCoordinates, size, color='b' ):
-  imageCoordintes = [ fieldCoordinates[0] / fieldsize[0] * fieldSizeInPixels[0] + topleft[0], \
-    fieldCoordinates[1] / fieldsize[1] * fieldSizeInPixels[1] + topleft[1] ]
+waypoints = np.array([ [5.0,10.0], [15.0,10.0], [30.0, 20.0]])
+
+def plotFromFieldCoordinates ( fieldCoordinates, size = 75, color='b' ):
+  imageCoordintes = [ (fieldCoordinates[0] / fieldsize[0] * fieldSizeInPixels[0]) + topleft[0], fieldCoordinates[1] / fieldsize[1] * fieldSizeInPixels[1] + topleft[1] ]
+  # print imageCoordintes
+  # print fieldCoordinates
+  # print (fieldCoordinates[0] / 54.00)
   plt.scatter(imageCoordintes[0], imageCoordintes[1], s=size, c=color)
 
 field = mpimg.imread('2019-field.png')
 imgplot = plt.imshow(field)
-plotFromFieldCoordinates([0,0], 100)
-plotFromFieldCoordinates([54,27], 100)
+
+
+
+
+
+for point in range(len(waypoints)):
+  # print waypoints[point][0]
+  # print waypoints[point][1]
+  print "waypoint: (%s, %s)" % (waypoints[point][0], waypoints[point][1])
+  plotFromFieldCoordinates([waypoints[point][0], waypoints[point][1]], color='r')
+
 
 plt.show()
 
