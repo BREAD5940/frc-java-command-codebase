77af016d2b0ef23433edf87f46ec051de1c4388c
Matthew Morley <matthew.morley.ca@gmail.com>
2019-01-09 22:28:54 -0800

    Update GrabHatch.java

--- src/main/java/frc/robot/auto/groups/GrabHatch.java
+++ src/main/java/frc/robot/auto/groups/GrabHatch.java
@@ -11,6 +11,13 @@ import java.util.ArrayList;
 public class GrabHatch extends CommandGroup{
     public GrabHatch() {
         //run whatever commands make the robot intake a hatch
+        /*
+        Idea for what should happen: Get to known distance from goal and set the wrist, go forward
+        to the distance such that the intake scrapes against the wall, intake while moving the elevator up,
+        and once the elevator achives a target height start backing up.
+        Start the elevator with a paralel command group, in which there is a waitForCondition command 
+        followed sequentailly by a setdrivetrainspeeds command.
+        */
     }
 
 }
