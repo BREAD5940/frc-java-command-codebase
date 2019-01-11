0f3a860bbfb55ed6c041246a49c9c3ad41a2f8d8
unduli-attacked <45951442+unduli-attacked@users.noreply.github.com>
2019-01-10 18:11:16 -0800

    begin vis translation to java

---
+++ src/main/PurePursuitVisualizer/Visualizer.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Visualizer{
+  ArrayList<Double> topLeft = new ArrayList<Double>(Array.asList(217,40));
+  ArrayList<Double> bottomRight = new ArrayList<Double>(Array.asList(1372, 615));
+  ArrayList<Double> fieldSize = new ArrayList<Double>(Array.asList(54, 27));
+  ArrayList<Double> fieldSizeInPixels = new ArrayList<Double>(Array.asList((bottomRight)));
+  ArrayList<Double> imageSize = new ArrayList<Double>(Array.asList(217,40));
+
+  ArrayList<Double> topLeft = new ArrayList<Double>(Array.asList(217,40));
+
+}
