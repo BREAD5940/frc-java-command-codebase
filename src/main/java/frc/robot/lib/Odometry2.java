236a880086e293ddc7679c023f2e08e58fb32a74
Matthew Morley <matthew.morley.ca@gmail.com>
2019-01-12 08:59:31 -0800

    Odometry 2: initial thoughts

--- src/main/java/frc/robot/lib/Odometry2.java
+++ src/main/java/frc/robot/lib/Odometry2.java
@@ -1,10 +1,14 @@
 package frc.robot.lib;
 
+import frc.math.Pose2d;
+import frc.math.Rotation2d;
+import frc.math.Translation2d;
 import frc.robot.Robot;
 
 public class Odometry2 {
-  double x_, y_, theta_;
+  // double x_, y_, theta_;
   double distance, deltaX, deltaY;
+  Pose2d pose_;
   
 
   /**
@@ -12,49 +16,51 @@ public class Odometry2 {
    * relative to the starting location of the robot plus a specified offset. If 
    * you set this to the "field center" coordinates odometry and Pure Persuit might^tm work
    * better, but it's likely fine.
+   * <p>
+   * his will set the robot pose to a x, y translation2d in feet and a theta in degrees.
+   * <p>
+   * @param x
+   * @param y
+   * @param theta
    */
   public Odometry2(double x, double y, double theta) {
-    this.x_ = x;
-    this.y_ = y;
-    this.theta_ = theta;
+    pose_ = new Pose2d(x, y, Rotation2d.fromDegrees(theta));
   }
 
   /**
-   * The Odometry2 class provides methods for tracking the location of the robot
-   * relative to the starting location of the robot.
+   * Put the robot at a pose2d of 0, 0, 0
    */
   public Odometry2() {
-    this.x_ = 0;
-    this.y_ = 0;
-    this.theta_ = 0;
-  }
-  
-
-  public void update(double leftDelta, double rightDelta, double globalTheta) {
-    distance = (leftDelta + rightDelta) / 2;
-    deltaX = distance * cosine(globalTheta);
-    deltaY = distance * sine(globalTheta);
+    this(0, 0, 0);
   }
 
   /**
-   * Simple method for calculating the cosine of an
-   * angle in degrees.
-   * @param theta in degrees
-   * @return the cosine of that angle
+   * Incrament the trajectory by a given Translation2d
+   * @param deltaMovement
    */
-  public double cosine(double theta) {
-    return Math.cos(Math.toRadians(theta));
+  public void incramentTrajectory(Translation2d deltaMovement) {
+    pose_ = new Pose2d( new Translation2d(pose_.getTranslation(), deltaMovement) , pose_.getRotation() );
   }
 
   /**
-   * Simple method for calculating the sine of an
-   * angle in degrees.
-   * @param theta in degrees
-   * @return the sin of that angle
+   * Get the x dispalcement of this' pose_
    */
-  public double sine(double theta) {
-    return Math.sin(Math.toRadians(theta));
+  public double getX() {
+    return pose_.getTranslation().x();
   }
 
+  /**
+   * Get the y dispalcement of this' pose_
+   */
+  public double getY() {
+    return pose_.getTranslation().y();
+  }
+
+  /**
+   * Get the global theta of this' pose_ in degrees
+   */
+  public double getTheta() {
+    return pose_.getRotation().getDegrees();
+  }
 
 }
