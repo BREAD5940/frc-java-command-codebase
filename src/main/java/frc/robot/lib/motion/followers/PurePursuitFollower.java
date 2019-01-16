7bbcb8b8dd5f39f73456b80934140426e81bfdf2
mcm001 <matthew.morley.ca@gmail.com>
2019-01-16 09:40:56 -0800

    Calculate curvature in pure pursuit follower
    Unit tests coming soon

--- src/main/java/frc/robot/lib/motion/followers/PurePursuitFollower.java
+++ src/main/java/frc/robot/lib/motion/followers/PurePursuitFollower.java
@@ -49,11 +49,54 @@ public class PurePursuitFollower {
     }
 
 
-    return null;
+    return mOutput;
   }
 
-  public double curvatureFromPathfinder(int index) {
-    return 0;
+  public double curvatureFromPathfinder(Trajectory path, int index) {
+
+    /*
+    To calculate curvature at a point we must first find the radius of 
+      the circle that intersects this point, the point ahead of it, and the point 
+      behind it. Once we have this, curvature is simply 1 / radius. From the pdf:
+    Given  P(x1, y1), Q(x2, y2), and R(x3, y3):
+    k  = 0.5 * ( x_1^2 + y_1^2 - Math.pow(x_2, 2) - y_^2)/(x_1 - x_2)
+    k_2 = (y_1 - y_2)/(x_1 - x_2)
+    b = 0.5 * (Math.pow(x_2, 2) - 2 * x_2 * k_1 + Math.pow(y_2, 2) - Math.pow(x_3, 2) + 2 * x_3 * k_1 - Math.pow(y_3, 2)) / (x_3 * k_2 - y_3 + y_2 - x_2 * k_2)
+    a = k_1 - k_2 * b
+    r = sqrt((x_1 - a)^2 + (y_1 - b)^2)
+    curvature = 1/r
+
+    However, consider that if x_1 = x_2 you divide by zero. (try adding 0.0001 to x?)
+    Also, if 1/r = NaN, the curvature is zero.
+    Furthermore, the first and last points don't have any preceding points, so just
+      set their curvature to zero.
+    */
+    double k_1, k_2, a, b, r;
+
+    if ( (index == path.length()) || (index == 0) ) {
+      return 0;
+    }
+
+    Point point1 = new Point(path.get(index-1).x,path.get(index-1).y);
+    Point point2 = new Point(path.get(index).x,path.get(index).y);
+    Point point3 = new Point(path.get(index+1).x,path.get(index+1).y);
+    
+    double x_1 = point1.x;
+    double x_2 = point2.x;
+    double x_3 = point3.x;
+
+    double y_1 = point1.y;
+    double y_2 = point2.y;
+    double y_3 = point3.y;
+
+
+    k_1  = 0.5 * ( Math.pow(x_1,2) + Math.pow(y_1,2) - Math.pow(x_2,2) - Math.pow(y_2,2))/(x_1 - x_2);
+    k_2 = (y_1 - y_2)/(x_1 - x_2);
+    b = 0.5 * (Math.pow(x_2, 2) - 2 * x_2 * k_1 + Math.pow(y_2, 2) - Math.pow(x_3, 2) + 2 * x_3 * k_1 - Math.pow(y_3, 2)) / (x_3 * k_2 - y_3 + y_2 - x_2 * k_2);
+    a = k_1 - k_2 * b;
+    r = Math.sqrt(Math.pow((x_1 - a),2) + Math.pow((y_1 - b),2));
+
+    return 1/r;
   }
 
   public double curvature(ArrayList<PurePoint> mTraj) {
