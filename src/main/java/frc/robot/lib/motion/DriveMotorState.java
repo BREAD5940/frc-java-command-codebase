a40b06730abbd72656b93d32e27a8199bff4f3f2
Matthew Morley <matthew.morley.ca@gmail.com>
2019-01-20 09:25:27 -0800

    Add javadocs to drivemotorstate

--- src/main/java/frc/robot/lib/motion/DriveMotorState.java
+++ src/main/java/frc/robot/lib/motion/DriveMotorState.java
@@ -12,7 +12,14 @@ public class DriveMotorState {
 	public boolean mBrakeMode;
 	public boolean mAccelMode;
 
-	
+	/**
+	 * This drivemotorstate is set using left and right velocities and accelerations in addition to a brakeMode bool
+	 * @param leftVel
+	 * @param leftAcc
+	 * @param rightVel
+	 * @param rightAcc
+	 * @param brakeMode
+	 */	
 	public DriveMotorState(double leftVel, double leftAcc, double rightVel, double rightAcc, boolean brakeMode) {
 		this.leftVel = leftVel;
 		this.leftAcc = leftAcc;
@@ -22,6 +29,13 @@ public class DriveMotorState {
 		this.mAccelMode = true;
 	}
 
+	/**
+	 * This drivemotorstate is not in brake mode and contains left and right velocities and accelerations
+	 * @param leftVel
+	 * @param leftAcc
+	 * @param rightVel
+	 * @param rightAcc
+	 */
 	public DriveMotorState(double leftVel, double leftAcc, double rightVel, double rightAcc) {
 		this.leftVel = leftVel;
 		this.leftAcc = leftAcc;
@@ -31,22 +45,35 @@ public class DriveMotorState {
 		this.mAccelMode = true;
 	}
 
+	/**
+	 * This drivemotorstate contains only left and right vels, and brake mode and acceleration control
+	 * is false.
+	 * @param leftVel
+	 * @param rightVel
+	 */
 	public DriveMotorState(double leftVel, double rightVel) {
 		this.leftVel = leftVel;
 		this.leftAcc = 0;
 		this.rightVel = rightVel;
 		this.rightAcc = 0;
 		this.mBrakeMode = false;
-		this.mAccelMode = true;
+		this.mAccelMode = false;
 	}
 
+	/**
+	 * Create a new drive motor state object with left and right velocities. In
+	 * this mode acceleration is not controlled and brake mode is user set.
+	 * @param leftVel
+	 * @param rightVel
+	 * @param brakeMode
+	 */
 	public DriveMotorState(double leftVel, double rightVel, boolean brakeMode) {
 		this.leftVel = leftVel;
 		this.leftAcc = 0;
 		this.rightVel = rightVel;
 		this.rightAcc = 0;
 		this.mBrakeMode = brakeMode;
-		this.mAccelMode = true;
+		this.mAccelMode = false;
 	}
 
 
