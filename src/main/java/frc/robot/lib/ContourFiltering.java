1fbc0c643f26f5bbb3dbf7079204e1f52c3743b9
Matthew Morley <matthew.morley.ca@gmail.com>
2019-01-20 16:05:28 -0800

    yeet

---
+++ src/main/java/frc/robot/lib/ContourFiltering.java
@@ -0,0 +1,12 @@
+package frc.robot.lib;
+
+/**
+ * A class for filtering the contours of vision targets
+ * posted to network tables by a grip pipeline running
+ * on the limelight. For now only supports
+ */
+public class ContourFiltering {
+
+  
+
+}
