76a6d842d966c23d5cc305b19f2d638302f03006
Matthew Morley <matthew.morley.ca@gmail.com>
2019-01-21 08:37:13 -0800

    Literally memes

---
+++ src/main/java/frc/robot/commands/subsystems/drivetrain/FollowPoseFromVisionTarget.java
@@ -0,0 +1,51 @@
+package frc.robot.commands.subsystems.drivetrain;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class FollowPoseFromVisionTarget extends Command {
+
+  boolean hadTarget;
+
+  double[] visionData, rangeData;
+
+  public FollowPoseFromVisionTarget() {
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+  }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void initialize() {
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+  @Override
+  protected void execute() {
+
+    // We first try to aquire a vision target
+    // this part doesn't need to run once you've found one
+    // for now the robot just sits and checks for a target,
+    // in the future make it active and try to find them,
+    // based on odometry?
+
+    
+
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
+  }
+}
