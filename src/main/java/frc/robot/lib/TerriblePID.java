27c44961d20168c3f66e8f52c8b04f3c00591490
mcm001 <matthew.morley.ca@gmail.com>
2019-01-30 18:06:32 -0800

    Copy paste terriblpid over

--- src/main/java/frc/robot/lib/TerriblePID.java
+++ src/main/java/frc/robot/lib/TerriblePID.java
@@ -18,88 +18,58 @@ public class TerriblePID {
 
   double kp, ki, kd, kf, pOutput, iOutput, fOutput, iAccum, dOutput, integralZone, 
     maxIntegralAccum, minOutput, maxOutput, setpoint, input, error, output;
-  double dt = 1 / 50; // Set delta time to 50 hz, this is likely good enough
+  double lastOutput, lastMeasured;
+  double dt = 1.0 / 50; // Set delta time to 50 hz, this is likely good enough
   FeedForwardMode feedforwardmode;
   FeedForwardBehavior feedforwardbehavior;
   /** The number of input units per quarter sine/cosine wave */
-  double unitsPerQuarterWave;
+  private double unitsPerQuarterWave;
+  private double maxAcceleration;
 
   /**
-   * Create a basic PI controller, sans the derivative term. When |error| is less than integralZone, 
+   * Create a basic PI controller, sans the derivative term. When |error| < integralZone, 
    * the Integral term will be active. If this is no longer true, the interal accum will
    * be flushed and the controller will effectively be a P controller. Slightly less
-   * shitty version of drivetrain.shitty_p_loop :P
+   * shitty version of drivetrain.shitty_p_loop. Use like this:
+   * <code>
+   * TerriblePID mPid = new TerriblePID(1 (kp), 0 (ki), 0 (kd), 0 (kf), -1 (min output), 1  (max output), 0  (integral zone), 1000  (max integral accum), 0 (ramp rate, null, null)
+   * </code>
    * @param kp gain
    * @param ki gain
+   * @param kd gain 
+   * @param kf
+   * @param minOutput
+   * @param maxOutput
    * @param integralZone about which integral will be active
    * @param maxIntegralAccum same as minimum accum, i term will top/bottom out here
+   * @param maxAcceleration
+   * @param FeedForwardMode
+   * @param FeedForwardBehavior
    */
-  public TerriblePID(double kp, double ki, double minOutput, double maxOutput, 
-    double integralZone, double maxIntegralAccum) {
+  public TerriblePID(double kp, double ki, double kd, double kf, double minOutput, double maxOutput, 
+    double integralZone, double maxIntegralAccum, double maxAcceleration, FeedForwardMode feedForwardMode,
+    FeedForwardBehavior feedforwardbehavior) {
       this.kp = kp;
       this.ki = ki;
-      this.minOutput = minOutput;
-      this.maxOutput = maxOutput;
-      this.integralZone = integralZone;
-      this.maxIntegralAccum = maxIntegralAccum;
-  }
-
-  /**
-   * Create a basic PI controller, sans the derivative term. When |error| is less than integralZone, 
-   * the Integral term will be active. If this is no longer true, the interal accum will
-   * be flushed and the controller will effectively be a P controller. Slightly less
-   * shitty version of drivetrain.shitty_p_loop :P 
-   * @param kp gain
-   * @param minOutput min output
-   * @param maxOutput max output
-   */
-  public TerriblePID(double kp, double minOutput, double maxOutput) {
-      this.kp = kp;
-      this.ki = 0;
-      this.minOutput = minOutput;
-      this.maxOutput = maxOutput;
-      this.integralZone = 0;
-      this.maxIntegralAccum = 0;
-  }
-
-  /**
-   * Create a basic PI controller, sans the derivative term. When |error| is less then integralZone, 
-   * the Integral term will be active. If this is no longer true, the interal accum will
-   * be flushed and the controller will effectively be a P controller. Slightly less
-   * shitty version of drivetrain.shitty_p_loop :P 
-   * This constructor also has support for different "shapes" of feedForward term. Depending on the 
-   * 
-   * @param kp gain
-   * @param ki gain
-   * @param integralZone about which integral will be active
-   * @param maxIntegralAccum same as minimum accum, i term will top/bottom out here
-   */
-  public TerriblePID(
-    double kp,
-    double ki,
-    double minOutput,
-    double maxOutput,
-    double integralZone,
-    double maxIntegralAccum,
-    double kf,
-    FeedForwardMode feedforwardmode,
-    FeedForwardBehavior feedforwardbehavior,
-    double unitsPerQuarterWave
-  ) {
-      this.kp = kp;
-      this.ki = ki;
-      this.minOutput = minOutput;
-      this.maxOutput = maxOutput;
-      this.integralZone = integralZone;
-      this.maxIntegralAccum = maxIntegralAccum;
-      this.feedforwardmode = feedforwardmode;
-      this.feedforwardbehavior = feedforwardbehavior;
+      this.kd = kd;
       this.kf = kf;
-      this.unitsPerQuarterWave = unitsPerQuarterWave;
+      this.minOutput = minOutput;
+      this.maxOutput = maxOutput;
+      this.integralZone = integralZone;
+      this.maxIntegralAccum = maxIntegralAccum;
+      this.feedforwardmode = feedForwardMode;
+      this.feedforwardbehavior = feedforwardbehavior;
+      this.maxAcceleration = maxAcceleration;
+      lastMeasured = 0;
   }
+
+  public TerriblePID(double kp, double maxOutput) {
+    this(kp, 0.0, 0.0, 0.0, -maxOutput, maxOutput, 0.0, 0.0, 0.0, null, null);
+  }
+
   /**
    * Set the setpoint for this instance of the PID loop. Should be preserved.
-   * @param demand new setpoint
+   * @param setpoint
    */
   public void setSetpoint(double demand) {
     setpoint = demand;
@@ -112,7 +82,7 @@ public class TerriblePID {
 
   /**
    * Set the maximum output of the controller
-   * @param max maximum output 
+   * @param maxOutput
    */
   public void setMaxOutput(double max) {
     maxOutput = max;
@@ -186,23 +156,41 @@ public class TerriblePID {
    * Calculates the feed forward term based on set settings
    */
   private double calculateFeedForward(double input) {
-    switch (feedforwardmode) {
-      case SINE:
-        fOutput = Math.sin(Math.toRadians( input * unitsPerQuarterWave ));
-      case COSINE:
-        fOutput = Math.cos(Math.toRadians( input * unitsPerQuarterWave ));
-      default:
-        fOutput = input * kf;
+    if ( feedforwardmode != null && feedforwardbehavior != null ) {
+
+      switch (feedforwardmode) {
+        case SINE:
+          fOutput = Math.sin(Math.toRadians( input * unitsPerQuarterWave ));
+          break;
+        case COSINE:
+          fOutput = Math.cos(Math.toRadians( input * unitsPerQuarterWave ));
+          break;
+        default:
+          fOutput = input * kf;
+      }
+      switch (feedforwardbehavior) {
+        case ALWAYSPOSITIVE:
+          fOutput = Math.abs(fOutput);
+          break;
+        case INVERTED:
+          fOutput = fOutput * -1;
+          break;
+        case NORMAL:
+          break;
+      }
+      return fOutput;
+    } else { // feedforward mode/behavor are null, return
+      return 0; 
     }
-    switch (feedforwardbehavior) {
-      case ALWAYSPOSITIVE:
-        fOutput = Math.abs(fOutput);
-      case INVERTED:
-        fOutput = fOutput * -1;
-      case NORMAL:
-        break;
+  }
+
+  private double ramp(double output, double previousOutput) {
+    if (output - previousOutput > maxAcceleration) {
+      output += maxAcceleration;
+    } else if (output - previousOutput < -maxAcceleration) {
+      output -= maxAcceleration;
     }
-    return fOutput;
+      return output;
   }
 
   /**
@@ -215,31 +203,53 @@ public class TerriblePID {
   public double update(double measured) {
     error = setpoint - measured;
 
-    /**
+    /*
      * P output is just the error times porportional gain
      */
     pOutput = kp * error;
 
-    System.out.println("ki is " + ki);
-    /**
-     * The iAccum should start at 0, but is incramented by error 
+    if (kd != 0) {
+      dOutput = -1 * kd * (measured - lastMeasured);
+    }
+    lastMeasured = measured;
+
+    /** The iAccum should start at 0, but is incramented by error 
      * times dt. This is then clamped to the minimum/maximum of 
      * the i term. This only happens if the integral gain is set.
      */
     if(ki != 0) {
       iAccum += error * ki * dt; // incrament the I term by error times integral gain times delta time (numerical integration yeet)
+      System.out.println(String.format("Error (%s) ki (%s) dt (%s) iAccum (%s)", error, ki, dt, iAccum));
       iAccum = clampIntegral(iAccum + iOutput); // clamp the term to the min/max   
     }
+    System.out.println("Ki: " + ki + " iAccum: " + iAccum);
 
-    if(kf != 0) {
+    if ((kf != 0) && (feedforwardbehavior != null) && (feedforwardmode != null)) {
       fOutput = calculateFeedForward(measured);
     }
 
-    /**
+    /*
      * This will make sure the output of the loop does not exceed the specified min/max.
      */
     output = clampOutput(pOutput + iAccum  + fOutput); 
 
+    if(maxAcceleration != 0) {
+      output = ramp(output, lastOutput);
+    }
+
+    lastOutput = output;
+
     return output;
   }
+
+
+  @Override
+	public String toString() {
+    try {
+      return String.format("Terrible PID Instance: Setpoint (%s) last measured (%s) kP (%s) kI (%s) kD (%s) kF (%s)  minOutput (%s) maxOutput (%s) iZone (%s) max integral accum (%s) maxAcceleration (%s) feedForwardMode (%s) feedForwardBehavior (%s) output (%s) last output (%s)", setpoint, lastMeasured, kp, ki, kd, kf, minOutput, maxOutput, integralZone, maxIntegralAccum, maxAcceleration, feedforwardmode.toString(), feedforwardbehavior.toString(), output, lastOutput);
+    } catch (Exception e) {
+      //TODO: handle exception
+      return "";
+    }
+	}
 }
