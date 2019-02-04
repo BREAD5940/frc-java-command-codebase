5770440636459c08e4e1132cfe2ac98a1d676c43
Matthew Morley <matthew.morley.ca@gmail.com>
2019-02-03 20:41:57 -0800

    Create runIntake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -7,6 +7,7 @@ import frc.robot.commands.subsystems.intake.IntakeTelop;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 
 /**
@@ -19,8 +20,8 @@ public class Intake extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-  public TalonSRX talon_left = new TalonSRX(RobotConfig.intake.left_intake_talon_port);
-  public TalonSRX talon_right = new TalonSRX(RobotConfig.intake.right_intake_talon_port);
+  public WPI_TalonSRX talon = new WPI_TalonSRX(RobotConfig.intake.left_intake_talon_port);
+  // public TalonSRX talon_right = new TalonSRX(RobotConfig.intake.right_intake_talon_port);
 
   float position_setpoint;
 
@@ -29,8 +30,7 @@ public class Intake extends Subsystem {
  * @param speed
  */
   public void setSpeed(double speed) {
-    talon_left.set(ControlMode.PercentOutput, speed);
-    talon_right.set(ControlMode.PercentOutput, -speed);
+    talon.set(ControlMode.PercentOutput, speed);
     SmartDashboard.putNumber("Intake speed setpoint", speed);
   }
 
