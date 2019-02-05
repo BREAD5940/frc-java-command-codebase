66cfac9063b3fdbd6257ec12c4fb6bfe912f6ec7
Matthew Morley <matthew.morley.ca@gmail.com>
2019-02-04 22:17:09 -0800

    move pointFinder to its own file for unit testing

--- src/main/java/frc/robot/subsystems/VisionProcessor.java
+++ src/main/java/frc/robot/subsystems/VisionProcessor.java
@@ -1,21 +1,16 @@
 package frc.robot.subsystems;
 
-import org.opencv.core.Point;
-import org.opencv.core.Point3;
-
-import java.util.ArrayList;
-
-import org.ghrobotics.lib.mathematics.twodim.geometry.Translation2d;
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfDouble;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.MatOfPoint3f;
-
+import org.opencv.core.Point3;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.lib.PointFinder;
 
 public class VisionProcessor {
 
@@ -80,95 +75,4 @@ public class VisionProcessor {
       System.out.println("rotationVector: " + rotationVector.dump());
       System.out.println("translationVector: " + translationVector.dump());
   }
-
-  public class PointFinder {
-
-    double[] xLoc, yLoc;
-    Translation2d topLeft, topRight, bottomLeft, bottomRight;
-
-    public PointFinder(double[] xCoords, double[] yCoords) {
-        xLoc = xCoords;
-        yLoc = yCoords;
-    }
-
-    public void calculate() {
-        // first, find the two coordinates that are at the bottom and the top
-        // point1, point2, point3, point4;
-        ArrayList<Translation2d> points = new ArrayList<Translation2d>();
-
-        points.add(new Translation2d(xLoc[0], yLoc[0]));
-        points.add(new Translation2d(xLoc[1], yLoc[1]));
-        points.add(new Translation2d(xLoc[2], yLoc[2]));
-        points.add(new Translation2d(xLoc[3], yLoc[3]));
-
-
-
-        ArrayList<Translation2d> bottomPoints = new ArrayList<Translation2d>();
-        ArrayList<Translation2d> topPoints = new ArrayList<Translation2d>();
-        Translation2d tempPoint = new Translation2d(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
-
-        // Loop through all the y coordinates, find the topmost one
-        for(int i=0; i<points.size(); i++) {
-            if(points.get(i).getY().getValue() > tempPoint.getY().getValue() ) tempPoint = points.get(i) ;
-        }
-        topPoints.add(tempPoint);
-        points.remove(tempPoint); // this should remove the current max from the list
-        tempPoint = new Translation2d(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
-
-        // Loop through all the y coordinates, find the topmost one
-        for(int i=0; i<points.size(); i++) {
-            if(points.get(i).getY().getValue() > tempPoint.getY().getValue() ) tempPoint = points.get(i) ;
-        }
-        topPoints.add(tempPoint);
-        points.remove(tempPoint); // this should remove the current max from the list
-
-        /* Now we should have only two left. Since we already added the two greatest points above, we know these are on the bottom. So just add them */
-        bottomPoints = points; // TODO check me that it works as intended
-
-
-        /* Now we have them ordered by Y coordinate, we should check what order they should be in in terms of X axis */
-        if(topPoints.get(0).getX().getValue() < topPoints.get(1).getX().getValue()) /* if the first index is more leftward than the second */ {
-            // we set the top left coordinate to index zero and to right to index one
-            topLeft = topPoints.get(0);
-            topRight = topPoints.get(1);
-        }
-        else {
-            // we have the inverse of above
-            topLeft = topPoints.get(1);
-            topRight = topPoints.get(0);
-        }
-
-        // We do the same thing for bottom points
-        if(bottomPoints.get(0).getX().getValue() < bottomPoints.get(1).getX().getValue()) /* if the first index is more leftward than the second */ {
-            // we set the top left coordinate to index zero and to right to index one
-            bottomLeft = topPoints.get(0);
-            bottomRight = topPoints.get(1);
-        }
-        else {
-            // we have the inverse of above
-            bottomLeft = bottomPoints.get(1);
-            bottomRight = bottomPoints.get(0);
-        }
-
-
-    }
-
-    public Point getTopLeft() {
-        return new Point(topLeft.getX().getValue(), topLeft.getY().getValue());
-    }
-
-    public Point getTopRight() {
-        return new Point(topRight.getX().getValue(), topRight.getY().getValue());
-    }
- 
-    public Point getBottomLeft() {
-        return new Point(bottomLeft.getX().getValue(), bottomLeft.getY().getValue());
-    }
-
-    public Point getBottomRight() {
-        return new Point(bottomRight.getX().getValue(), bottomRight.getY().getValue());
-    }
-
-  }
-
 }
