29a95cf287e00e770de70fe5fb9ae1007a0900b8
unduli-attacked <45951442+unduli-attacked@users.noreply.github.com>
2019-02-07 14:40:51 -0800

    comment things out until the tests don't fail

--- src/main/java/frc/robot/commands/auto/FieldConstraints.java
+++ src/main/java/frc/robot/commands/auto/FieldConstraints.java
@@ -50,59 +50,70 @@ public class FieldConstraints {
     System.out.print("Minimum y: ");
     System.out.println(minY.getFeet());
 
-    for(int i=0; i<points.size()-1; i++){
-      Translation2d point = points.get(i).getState().getPose().getTranslation();
-      Length safeX = point.getX();
-      Length safeY = point.getY();
-      
+    for(int i=0; i<points.size(); i++){
+      if(i==0||i==points.size()-1){
+        System.out.println("No correction required!");
+        safePoints.add(points.get(i));
+      }else{
+        Translation2d point = points.get(i).getState().getPose().getTranslation();
+        Length safeX = point.getX();
+        Length safeY = point.getY();
+        
 
-      // point = new Translation2d(safeX, safeY); //set the current point to the safepoint inside the field
+        point = new Translation2d(safeX, safeY); //set the current point to the safepoint inside the field
 
-      // for(int j=0; j<constraints.size()-1; j++){
-      //   if(!(point.getX().getFeet()>constraints.get(j)[0].getX().getFeet()&&point.getX().getFeet()<constraints.get(j)[1].getX().getFeet()
-      //         &&point.getY().getFeet()>constraints.get(j)[0].getY().getFeet()&&point.getX().getFeet()<constraints.get(j)[1].getX().getFeet())){
-      //     //theoretically picks the point on the border closest to the original point
-      //     Translation2d lastNearest = constraints.get(j)[0];
-      //     double lastShortest = distanceFormula(lastNearest, point);
-      //     double precision;
-      //     if(bigSpeed){
-      //       precision=1;
-      //     }else{
-      //       precision=0.1;
-      //     }
-      //     for (double x=0; x<Math.abs(constraints.get(j)[0].getX().getFeet()-constraints.get(j)[1].getX().getFeet()); x+=precision){ //IMPORTANT this currently makes the whole thing take about
-      //       for (double y=0; y<Math.abs(constraints.get(j)[0].getY().getFeet()-constraints.get(j)[1].getY().getFeet()); y+=precision){//20sec longer to execute. we change it to 1, it's less precise, but faster
-      //         if(distanceFormula(point, new Translation2d(x, y))<lastShortest){lastNearest=new Translation2d(x, y);}
-      //       }
-      //     }
-      //     safeX=lastNearest.getX();
-      //     safeY=lastNearest.getY();
-      //   }
-      // }
+        // for(int j=0; j<constraints.size()-1; j++){
+        //   if(!(point.getX().getFeet()>constraints.get(j)[0].getX().getFeet()&&point.getX().getFeet()<constraints.get(j)[1].getX().getFeet()
+        //         &&point.getY().getFeet()>constraints.get(j)[0].getY().getFeet()&&point.getX().getFeet()<constraints.get(j)[1].getX().getFeet())){
+        //     //theoretically picks the point on the border closest to the original point
+        //     //FIXME so this is the bit that makes it only be 53
+        //     Translation2d lastNearest = constraints.get(j)[0];
+        //     double lastShortest = distanceFormula(lastNearest, point);
+        //     double precision;
+        //     if(bigSpeed){
+        //       precision=1;
+        //     }else{
+        //       precision=0.1;
+        //     }
+        //     for (double x=0; x<Math.abs(constraints.get(j)[0].getX().getFeet()-constraints.get(j)[1].getX().getFeet()); x+=precision){ //IMPORTANT this currently makes the whole thing take about
+        //       for (double y=0; y<Math.abs(constraints.get(j)[0].getY().getFeet()-constraints.get(j)[1].getY().getFeet()); y+=precision){//20sec longer to execute. we change it to 1, it's less precise, but faster
+        //         if(distanceFormula(point, new Translation2d(x, y))<lastShortest){lastNearest=new Translation2d(x, y);}
+        //       }
+        //     }
+        //     safeX=lastNearest.getX();
+        //     safeY=lastNearest.getY();
+        //     System.out.println(safeX.getFeet());
+        //     System.out.println(safeY.getFeet());
+        //   }
+        // }
 
-      if(point.getX().getFeet()>maxX.getFeet()){
-        safeX=maxX;
-        System.out.print("safed to max x. safeX: ");
-        System.out.println(safeX);
-      }
-      if(point.getX().getFeet()<minX.getFeet()){
-        safeX=minX;
-        System.out.print("safed to min x. safeX: ");
-        System.out.println(safeX);
-      }
-      if(point.getY().getFeet()>maxY.getFeet()){
-        safeY=maxY;
-        System.out.print("safed to max y. safeY: ");
-        System.out.println(safeY);
-      }
-      if(point.getY().getFeet()<minY.getFeet()){
-        safeY=minY;
-        System.out.print("safed to min Y. safeY: ");
-        System.out.println(safeY);
-      }
+        // point = new Translation2d(safeX, safeY); //set the current point to the safepoint inside the field
 
-      safePoints.add(i,new TimedEntry<Pose2dWithCurvature>((new Pose2dWithCurvature(new Pose2d(new Translation2d(safeX,safeY),points.get(i).getState().getPose().getRotation()),points.get(i).getState().getCurvature())),
-                        points.get(i).getT(), points.get(i).getVelocity(), points.get(i).getAcceleration()));
+
+        // if(point.getX().getFeet()>maxX.getFeet()){
+        //   safeX=maxX;
+        //   System.out.print("safed to max x. safeX: ");
+        //   System.out.println(safeX);
+        // }
+        // if(point.getX().getFeet()<minX.getFeet()){
+        //   safeX=minX;
+        //   System.out.print("safed to min x. safeX: ");
+        //   System.out.println(safeX);
+        // }
+        // if(point.getY().getFeet()>maxY.getFeet()){
+        //   safeY=maxY;
+        //   System.out.print("safed to max y. safeY: ");
+        //   System.out.println(safeY);
+        // }
+        // if(point.getY().getFeet()<minY.getFeet()){
+        //   safeY=minY;
+        //   System.out.print("safed to min Y. safeY: ");
+        //   System.out.println(safeY);
+        // }
+
+        safePoints.add(i,new TimedEntry<Pose2dWithCurvature>((new Pose2dWithCurvature(new Pose2d(new Translation2d(safeX,safeY),points.get(i).getState().getPose().getRotation()),points.get(i).getState().getCurvature())),
+                          points.get(i).getT(), points.get(i).getVelocity(), points.get(i).getAcceleration()));
+      }
     }
 
     System.out.println(safePoints.get(0).getState().getPose().getTranslation().getX().getFeet());
@@ -114,10 +125,11 @@ public class FieldConstraints {
     System.out.println(dos[0][0]);
     System.out.println(dos[0][1]);
     List<TimedEntry<Pose2dWithCurvature>> tres = doublesAsPoints(safePoints, dos);
+
     System.out.println(tres.get(0).getState().getPose().getTranslation().getX().getFeet());
     System.out.println(tres.get(0).getState().getPose().getTranslation().getY().getFeet());
     //TODO test to see if this smoother actually works
-    TimedTrajectory<Pose2dWithCurvature> toReturn = new TimedTrajectory<Pose2dWithCurvature>(tres, false);
+    TimedTrajectory<Pose2dWithCurvature> toReturn = new TimedTrajectory<Pose2dWithCurvature>(safePoints, false);
     System.out.println(toReturn.getPoints().get(0).getState().getPose().getTranslation().getX().getFeet());
     System.out.println(toReturn.getPoints().get(0).getState().getPose().getTranslation().getY().getFeet());
     return toReturn;
@@ -182,31 +194,35 @@ public class FieldConstraints {
     List<TimedEntry<Pose2dWithCurvature>> toReturn = new ArrayList<TimedEntry<Pose2dWithCurvature>>();
 
     for (int i=0; i<newP.length; i++){
-      double curve=0;
-      // if(i==0||i==newP.length-1){
-      //   curve=0;
-      // }else{
-      //   if(newP[i-1][0]==newP[i][0]){
-      //     newP[i-1][0]+=0.001;
-      //   }
-      //   double k1=0.5*(Math.pow(newP[i-1][0],2)+Math.pow(newP[i-1][1],2)-Math.pow(newP[i][0],2)-Math.pow(newP[i][1],2))/(newP[i-1][0]-newP[i][0]);
-      //   double k2=(newP[i-1][1]-newP[i][1])/(newP[i-1][0]-newP[i][0]);
-      //   double b=0.5*(Math.pow(newP[i][0],2)-2*newP[i][0]*k1+Math.pow(newP[i][1],2)-Math.pow(newP[i+1][0],2)+2*newP[i+1][0]*k1-Math.pow(newP[i+1][1],2))
-      //       /(newP[i+1][0]*k2-newP[i+1][1]+newP[i][1]-newP[i][1]*k2);
-      //   double a=k1-k2*b;
-      //   double r=Math.sqrt(Math.pow((newP[i-1][1]-a),2)+Math.pow((newP[i-1][1]-b),2));
-      //   curve = 1/r;
-      // }
-      //FIXME i don't know what the deriv of the curvature is, so im leaving it the same
-      Pose2dCurvature newCurve = new Pose2dCurvature(curve, original.get(i).getState().getCurvature().getDkds());
-      Rotation2d newRot=new Rotation2d(0);
-      // if(i==0){
-      //   newRot=original.get(i).getState().getPose().getRotation(); //just set it to the original
-      // }else{
-      //   newRot= new Rotation2d((newP[i-1][1]-newP[i][1])/(newP[i-1][0]-newP[i][0])); //this is just the secant between the current pt and before
-      // }
-      toReturn.add(i,new TimedEntry<Pose2dWithCurvature>((new Pose2dWithCurvature(new Pose2d(new Translation2d(newP[i][0],newP[i][1]),newRot),
-            newCurve)), original.get(i).getT(), original.get(i).getVelocity(), original.get(i).getAcceleration()));
+      if(i==0||i==newP.length-1){
+        toReturn.add(original.get(i));
+      }else{
+        double curve=original.get(i).getState().getCurvature().get_curvature$FalconLibrary();
+        // if(i==0||i==newP.length-1){
+        //   curve=0;
+        // }else{
+        //   if(newP[i-1][0]==newP[i][0]){
+        //     newP[i-1][0]+=0.001;
+        //   }
+        //   double k1=0.5*(Math.pow(newP[i-1][0],2)+Math.pow(newP[i-1][1],2)-Math.pow(newP[i][0],2)-Math.pow(newP[i][1],2))/(newP[i-1][0]-newP[i][0]);
+        //   double k2=(newP[i-1][1]-newP[i][1])/(newP[i-1][0]-newP[i][0]);
+        //   double b=0.5*(Math.pow(newP[i][0],2)-2*newP[i][0]*k1+Math.pow(newP[i][1],2)-Math.pow(newP[i+1][0],2)+2*newP[i+1][0]*k1-Math.pow(newP[i+1][1],2))
+        //       /(newP[i+1][0]*k2-newP[i+1][1]+newP[i][1]-newP[i][1]*k2);
+        //   double a=k1-k2*b;
+        //   double r=Math.sqrt(Math.pow((newP[i-1][1]-a),2)+Math.pow((newP[i-1][1]-b),2));
+        //   curve = 1/r;
+        // }
+        //FIXME i don't know what the deriv of the curvature is, so im leaving it the same
+        Pose2dCurvature newCurve = new Pose2dCurvature(curve, original.get(i).getState().getCurvature().getDkds());
+        Rotation2d newRot=new Rotation2d(0);
+        // if(i==0){
+        //   newRot=original.get(i).getState().getPose().getRotation(); //just set it to the original
+        // }else{
+        //   newRot= new Rotation2d((newP[i-1][1]-newP[i][1])/(newP[i-1][0]-newP[i][0])); //this is just the secant between the current pt and before
+        // }
+        toReturn.add(i,new TimedEntry<Pose2dWithCurvature>((new Pose2dWithCurvature(new Pose2d(new Translation2d(newP[i][0],newP[i][1]),newRot),
+              newCurve)), original.get(i).getT(), original.get(i).getVelocity(), original.get(i).getAcceleration()));
+      }
     }
 
     return toReturn;
