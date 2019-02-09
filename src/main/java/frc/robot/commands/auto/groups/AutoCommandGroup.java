afeae8dd449514289a217c337fc01e7c43f7044b
mcm001 <matthew.morley.ca@gmail.com>
2019-02-09 14:59:17 -0800

    Update AutoCommandGroup.java

--- src/main/java/frc/robot/commands/auto/groups/AutoCommandGroup.java
+++ src/main/java/frc/robot/commands/auto/groups/AutoCommandGroup.java
@@ -1,11 +1,26 @@
 package frc.robot.commands.auto.groups;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc.robot.lib.Logger;
 
 /**
  * basically just a CommandGroup but with the done() method
  */
 public class AutoCommandGroup extends CommandGroup {
+
+	double start = 0;
+
+	@Override
+	public synchronized void start() {
+		super.start();
+		start = Timer.getFPGATimestamp();
+	}
+
+	@Override
+	protected void end() {
+    Logger.log("Path ran in " + (Timer.getFPGATimestamp() - start) + " seconds!");
+	}
 
 	public AutoCommandGroup() {}
 
