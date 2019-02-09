17da47d46d92d60f3592e8d4dec22a4b84663954
unduli-attacked <45951442+unduli-attacked@users.noreply.github.com>
2019-02-09 12:36:37 -0800

    change height of path

--- src/main/java/frc/robot/commands/auto/Trajectories.java
+++ src/main/java/frc/robot/commands/auto/Trajectories.java
@@ -122,7 +122,7 @@ public class Trajectories {
 
 
     generatedTrajectories.put("test",generateTrajectory(new ArrayList<Pose2d>(Arrays.asList(locations.get("loadingL"),
-        new Pose2d(LengthKt.getFeet(11), LengthKt.getFeet(21), Rotation2dKt.getDegree(145)),
+        new Pose2d(LengthKt.getFeet(11.71), LengthKt.getFeet(20.5), Rotation2dKt.getDegree(145)),
           new Pose2d(LengthKt.getFeet(11.71), LengthKt.getFeet(21.119), Rotation2dKt.getDegree(-35)))), true));
     generatedTrajectories.put("test1",generateTrajectory(Arrays.asList(new Pose2d(LengthKt.getFeet(11.71), LengthKt.getFeet(21.119), Rotation2dKt.getDegree(-35)), 
         locations.get("cargoL1")), false));
