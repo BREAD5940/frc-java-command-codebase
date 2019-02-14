a0fbd6db46ac4099d96d6c6a5fd56fdb49ff34a8
unduli-attacked <45951442+unduli-attacked@users.noreply.github.com>
2019-02-13 20:52:23 -0800

    make the command actually do at least 1 thing

--- src/main/java/frc/robot/commands/subsystems/superstructure/SuperstructureGoToState.java
+++ src/main/java/frc/robot/commands/subsystems/superstructure/SuperstructureGoToState.java
@@ -2,29 +2,32 @@ package frc.robot.commands.subsystems.superstructure;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.states.SuperStructureState;
+import frc.robot.subsystems.superstructure.SuperStructure;
 
 public class SuperstructureGoToState extends Command {
 	SuperStructureState mRequState;
 
 	public SuperstructureGoToState(SuperStructureState requState) {
-		// requires(SuperStructure.getInstance());
+		requires(SuperStructure.getInstance());
 		mRequState = requState;
 	}
 
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
-		// SuperStructure.getInstance().setReqState(mRequState);
+		SuperStructure.getInstance().setReqState(mRequState);
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
-	protected void execute() {}
+	protected void execute() {
+		//do nothing? i guess?
+	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return true;
+		return (mRequState.getElevatorHeight().getInch()-SuperStructure.getInstance().updateState().getElevatorHeight().getInch())<=2;
 	}
 
 	// Called once after isFinished returns true
