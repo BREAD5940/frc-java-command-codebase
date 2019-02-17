fa57f2c92fee6dd59fc829eea1503580eaa50b59
Matthew Morley <matthew.morley.ca@gmail.com>
2019-02-16 19:24:05 -0800

    i mean I guess it works kinda maybe, but not with our code right now

--- src/main/java/frc/robot/subsystems/superstructure/SuperStructure.java
+++ src/main/java/frc/robot/subsystems/superstructure/SuperStructure.java
@@ -264,7 +264,7 @@ public class SuperStructure extends Subsystem {
 
 		SmartDashboard.putNumber("elevator height in inches", mCurrentState.elevator.getHeight().getInch());
 		SmartDashboard.putNumber("target elevator height", mReqPath.get(0).getElevator().height.getInch());
-		SmartDashboard.putNumber("elevator output", getElevator().getMaster().getMotorOutputVoltage());
+		// SmartDashboard.putNumber("elevator output", getElevator().getMaster().getMotorOutputVoltage()); // commented out because it breaks sim
 
 		SmartDashboard.putNumber("Wrist position", getWrist().getPosition().getDegree());
 	}
