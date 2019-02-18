822a0bc2d3be370555fb258036514ff4c1a28b4e
mcm001 <matthew.morley.ca@gmail.com>
2019-02-18 01:23:03 -0800

    Make go to state actually go to a state

--- src/main/java/frc/robot/commands/subsystems/superstructure/SuperstructureGoToState.java
+++ src/main/java/frc/robot/commands/subsystems/superstructure/SuperstructureGoToState.java
@@ -1,5 +1,7 @@
 package frc.robot.commands.subsystems.superstructure;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.states.SuperStructureState;
 import frc.robot.subsystems.superstructure.SuperStructure;
@@ -14,6 +16,12 @@ public class SuperstructureGoToState extends Command {
 		setTimeout(kDefaultTimeout);
 	}
 
+	public SuperstructureGoToState(SuperStructureState requState, double timeout) {
+		requires(SuperStructure.getInstance());
+		mRequState = requState;
+		setTimeout(timeout);
+	}
+
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
@@ -23,15 +31,19 @@ public class SuperstructureGoToState extends Command {
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
-		//do nothing? i guess?
+		// if (Math.abs(mOI.getWristAxis()) > 0.07) {
+			SuperStructure.getInstance().getWrist().getMaster().set(ControlMode.Position, mRequState.getWrist().angle);
+
+			SuperStructure.getInstance().getElbow().getMaster().set(ControlMode.Position, mRequState.getElbow().angle);
+
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return (Math.abs(mRequState.getElevatorHeight().getInch() - SuperStructure.getInstance().updateState().getElevatorHeight().getInch()) <= 0.5)
+		return ((Math.abs(mRequState.getElevatorHeight().getInch() - SuperStructure.getInstance().updateState().getElevatorHeight().getInch()) <= 0.5)
 				&& (Math.abs(mRequState.getElbow().angle.getDegree() - SuperStructure.getInstance().updateState().getElbow().angle.getDegree()) <= 5) //FIXME check tolerences
-				&& (Math.abs(mRequState.getWrist().angle.getDegree() - SuperStructure.getInstance().updateState().getWrist().angle.getDegree()) <= 5);
+				&& (Math.abs(mRequState.getWrist().angle.getDegree() - SuperStructure.getInstance().updateState().getWrist().angle.getDegree()) <= 5)) || isTimedOut();
 	}
 
 	// Called once after isFinished returns true
