5ed26ed7d04e295f3f5ff0c756e6ffba9dd4292e
mcm001 <matthew.morley.ca@gmail.com>
2019-02-20 10:20:09 -0800

    More vision - measure distance?

--- src/main/java/frc/robot/lib/obj/factories/VisionTargetFactory.java
+++ src/main/java/frc/robot/lib/obj/factories/VisionTargetFactory.java
@@ -11,12 +11,20 @@ public class VisionTargetFactory {
 
 	private static final Length kRocketCargoTapeHeight = LengthKt.getInch(20); //distance from floor to bottom of tape TODO I don't think we need this
 
-	public static VisionTarget getRocketCargoTarget() {
-		return new VisionTarget(kRocketCargoTapeHeight);
+	public static VisionTarget getRocketCargoDualTarget() {
+		return new VisionTarget(VisionTarget.kDualVisionTapeShape, kRocketCargoTapeHeight);
 	}
 
-	public static VisionTarget getHatchTarget() {
-		return new VisionTarget(kHatchTapeHeight);
+	public static VisionTarget getHatchDualTarget() {
+		return new VisionTarget(VisionTarget.kDualVisionTapeShape, kHatchTapeHeight);
+	}
+
+	public static VisionTarget getRocketCargoSingleTarget() {
+		return new VisionTarget(VisionTarget.kSingleVisionTapeShape, kRocketCargoTapeHeight);
+	}
+
+	public static VisionTarget getHatchSingleTarget() {
+		return new VisionTarget(VisionTarget.kSingleVisionTapeShape, kHatchTapeHeight);
 	}
 
 }
