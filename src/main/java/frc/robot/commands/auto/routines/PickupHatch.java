f3d7c72f0a8195fe7d56313e6574626e1b06a776
Matthew Morley <matthew.morley.ca@gmail.com>
2019-02-23 22:19:56 -0800

    Update PickupHatch.java
    it will now use drivedistancethesecond

--- src/main/java/frc/robot/commands/auto/routines/PickupHatch.java
+++ src/main/java/frc/robot/commands/auto/routines/PickupHatch.java
@@ -1,8 +1,10 @@
 package frc.robot.commands.auto.routines;
 
+import org.ghrobotics.lib.mathematics.units.LengthKt;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.RobotConfig.auto.fieldPositions;
-import frc.robot.commands.subsystems.drivetrain.DriveDistance;
+import frc.robot.commands.subsystems.drivetrain.DriveDistanceTheSecond;
 import frc.robot.commands.subsystems.drivetrain.FollowVisionTarget;
 import frc.robot.commands.subsystems.superstructure.RunIntake;
 import frc.robot.commands.subsystems.superstructure.SuperstructureGoToState;
@@ -10,6 +12,7 @@ import frc.robot.states.ElevatorState;
 import frc.robot.subsystems.superstructure.SuperStructure.iPosition;
 
 public class PickupHatch extends CommandGroup {
+
 	/**
 	 * Add your docs here.
 	 */
@@ -17,10 +20,10 @@ public class PickupHatch extends CommandGroup {
 
 		addParallel(new SuperstructureGoToState(new ElevatorState(fieldPositions.hatchLowGoal), iPosition.HATCH));
 
-		addSequential(new FollowVisionTarget(1, 5, 10));
+		addSequential(new FollowVisionTarget(1, 6, 10));
 
 		addSequential(new RunIntake(-1, 0.5));
 
-		addSequential(new DriveDistance(-0.5));
+		addSequential(new DriveDistanceTheSecond(LengthKt.getFeet(.5), true));
 	}
 }
