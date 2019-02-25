a14e6225d2b0c137ab4636e3686669871841e859
Matthew Morley <matthew.morley.ca@gmail.com>
2019-02-24 19:40:59 -0800

    Make passthrough command groups and One Button to Rule them All

---
+++ src/main/java/frc/robot/commands/auto/routines/passthrough/PassThrough.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands.auto.routines.passthrough;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.ConditionalCommand;
+import edu.wpi.first.wpilibj.command.PrintCommand;
+import frc.robot.subsystems.superstructure.SuperStructure;
+
+public class PassThrough extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public PassThrough() {
+    // addSequential( new ConditionalCommand(new PassThroughForward(), new PassThroughReverse()){
+    //   @Override
+    //   protected boolean condition() {
+    //     SuperStructure.getInstance().updateState();
+    //     return (SuperStructure.getInstance().getCurrentState().jointAngles.elbowAngle.angle.getDegree() > -90 );
+    //   }
+    // });
+
+    Command decider = new ConditionalCommand(new PassThroughForward(), new PassThroughReverse()){
+      @Override
+      protected boolean condition() {
+        SuperStructure.getInstance().updateState();
+        double degrees = SuperStructure.getInstance().getCurrentState().jointAngles.elbowAngle.angle.getDegree();
+        boolean direction = (degrees > -90 );
+        return (SuperStructure.getInstance().getCurrentState().jointAngles.elbowAngle.angle.getDegree() > -90 );
+      }};
+    
+    addSequential(decider);
+
+  }
+}
--- src/main/java/frc/robot/commands/auto/routines/passthrough/PassThroughForward.java
+++ src/main/java/frc/robot/commands/auto/routines/passthrough/PassThroughForward.java
@@ -5,13 +5,11 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands.auto.routines;
+package frc.robot.commands.auto.routines.passthrough;
 
 import org.ghrobotics.lib.mathematics.units.LengthKt;
-import org.ghrobotics.lib.mathematics.units.TimeUnitsKt;
 
-import edu.wpi.first.wpilibj.command.PrintCommand;
-import frc.robot.commands.auto.actions.DelayCommand;
+import frc.robot.RobotConfig.auto.fieldPositions;
 import frc.robot.commands.auto.groups.AutoCommandGroup;
 import frc.robot.commands.subsystems.superstructure.SuperstructureGoToState;
 import frc.robot.lib.obj.RoundRotation2d;
@@ -19,45 +17,25 @@ import frc.robot.planners.SuperstructurePlanner;
 import frc.robot.states.ElevatorState;
 import frc.robot.states.SuperStructureState;
 import frc.robot.subsystems.superstructure.RotatingJoint.RotatingArmState;
+import frc.robot.subsystems.superstructure.SuperStructure.iPosition;
 
-public class PassThrough extends AutoCommandGroup {
+public class PassThroughForward extends AutoCommandGroup {
 	/**
-	 * Add your docs here.
+	 * Pass through the elevator from front to back
 	 */
-	public PassThrough() {
-		// 25.987168527570645,-99.67531651929261,-58.062744140625,NONE
+	public PassThroughForward() {
 
 		SuperStructureState passthroughInitState = SuperstructurePlanner.passThroughState;
-		// 24.692488742595167,-179.08774618167203,-117.97119140625,NONE
 
 		SuperStructureState passedThrough = new SuperStructureState(
 				new ElevatorState(LengthKt.getInch(26)),
 				new RotatingArmState(RoundRotation2d.getDegree(-180)),
 				new RotatingArmState(RoundRotation2d.getDegree(-120)));
 
+		addSequential(new SuperstructureGoToState(new ElevatorState(fieldPositions.hatchLowGoal), iPosition.HATCH));
 		addSequential(new SuperstructureGoToState(passthroughInitState));
-		addSequential(new PrintCommand("At the bottom - waiting..."));
-		addSequential(new DelayCommand(TimeUnitsKt.getSecond(1)));
-		// addSequential(new SuperstructureGoToState(straightDown));
-		// addSequential(new DelayCommand(TimeUnitsKt.getSecond(2)));
-		addSequential(new PrintCommand("Passing Though..."));
 		addSequential(new SuperstructureGoToState(passedThrough));
+		addSequential(new SuperstructureGoToState(new ElevatorState(fieldPositions.hatchLowGoal), iPosition.HATCH_REVERSE));
 
-		// Add Commands here:
-		// e.g. addSequential(new Command1());
-		// addSequential(new Command2());
-		// these will run in order.
-
-		// To run multiple commands at the same time,
-		// use addParallel()
-		// e.g. addParallel(new Command1());
-		// addSequential(new Command2());
-		// Command1 and Command2 will run in parallel.
-
-		// A command group will require all of the subsystems that each member
-		// would require.
-		// e.g. if Command1 requires chassis, and Command2 requires arm,
-		// a CommandGroup containing them would require both the chassis and the
-		// arm.
 	}
 }
