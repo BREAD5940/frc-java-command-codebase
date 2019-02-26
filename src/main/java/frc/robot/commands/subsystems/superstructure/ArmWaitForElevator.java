80671ba70f125b1c78e410a2f41ccdb32c72f980
Matt <matthew.morley.ca@gmail.com>
2019-02-26 12:19:12 -0800

    Format arm wait for elevator, still a Callable

--- src/main/java/frc/robot/commands/subsystems/superstructure/ArmWaitForElevator.java
+++ src/main/java/frc/robot/commands/subsystems/superstructure/ArmWaitForElevator.java
@@ -1,48 +1,39 @@
 package frc.robot.commands.subsystems.superstructure;
 
+import java.util.concurrent.Callable;
+
 import org.ghrobotics.lib.mathematics.units.Length;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.lib.obj.RoundRotation2d;
+import frc.robot.commands.auto.groups.AutoCommandGroup;
+import frc.robot.lib.AutoWaitForCondition;
 import frc.robot.states.IntakeAngle;
-import frc.robot.subsystems.superstructure.Elevator;
-import frc.robot.subsystems.superstructure.RotatingJoint.RotatingArmState;
 import frc.robot.subsystems.superstructure.SuperStructure;
 
-public class ArmWaitForElevator extends Command{
+public class ArmWaitForElevator extends AutoCommandGroup {
 
-  IntakeAngle desired;
-  Length finalEleHeight, tolerence;
-  boolean isDescending;
+	IntakeAngle desired;
+	Length finalEleHeight, tolerence;
+	boolean isDescending;
+	Callable<Boolean> elevatorMoved;
+	Command mDelayedCommand;
 
-  public ArmWaitForElevator(IntakeAngle desired, Length finalEleHeight, Length tolerence, boolean isDescending){
-    this.desired = desired;
-    this.finalEleHeight = finalEleHeight;
-    this.tolerence = tolerence;
-    this.isDescending = isDescending;
-  }
+	public ArmWaitForElevator(IntakeAngle desired, Length finalEleHeight, Length tolerence, boolean isDescending) {
+		elevatorMoved = (() -> {
+			return ((SuperStructure.getElevator().getHeight().getInch() < finalEleHeight.plus(tolerence).getInch() && isDescending)
+					|| (SuperStructure.getElevator().getHeight().getInch() > finalEleHeight.minus(tolerence).getInch()
+							&& !isDescending));
+		}); // this lamda Caller can be read as:
+		// (null) -> [or becomes, turns into] (basically if the elevator is within tolerance)
+		// this Caller is then used by auto wait for condition and polled in isFinished();
 
-  @Override
-  protected void initialize(){
-    clearRequirements(); //make sure we aren't bothering anyone
-  }
+		addSequential(new AutoWaitForCondition(elevatorMoved));
+		addSequential(new ArmMove(desired));
+	}
 
-  @Override
-  protected void execute(){
-    if((SuperStructure.getElevator().getHeight().getInch() < finalEleHeight.plus(tolerence).getInch() && isDescending) 
-        || (SuperStructure.getElevator().getHeight().getInch() > finalEleHeight.minus(tolerence).getInch()
-            && !isDescending)) {
-      requires(SuperStructure.getInstance()); // I'm still really confused about the whole superstructure is a subsystem BUT ALSO everything in it is a subsystem. Maybe we should just choose one?
-		  requires(SuperStructure.getInstance().getWrist());
-		  requires(SuperStructure.getInstance().getElbow());
-      SuperStructure.getInstance().getWrist().requestAngle(desired.getWrist().angle);
-      SuperStructure.getInstance().getElbow().requestAngle(desired.getElbow().angle);
-    }
-  }
-
-  @Override
-  protected boolean isFinished() {
-    return false;
-  }
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
 
 }
