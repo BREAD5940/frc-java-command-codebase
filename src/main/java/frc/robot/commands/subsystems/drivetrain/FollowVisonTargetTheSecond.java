edbecb6008437ab0148746b0f28812de5c3b071f
Matt <matthew.morley.ca@gmail.com>
2019-03-02 01:11:14 -0800

    Update to have an isDone() that works better, tune PID

--- src/main/java/frc/robot/commands/subsystems/drivetrain/FollowVisonTargetTheSecond.java
+++ src/main/java/frc/robot/commands/subsystems/drivetrain/FollowVisonTargetTheSecond.java
@@ -10,10 +10,11 @@ package frc.robot.commands.subsystems.drivetrain;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.superstructure.SuperStructure;
 
 public class FollowVisonTargetTheSecond extends Command {
 
-	double targetArea = 4.5;
+	double targetArea;
 
 	public FollowVisonTargetTheSecond(double targetArea) {
 		requires(DriveTrain.getInstance());
@@ -22,7 +23,10 @@ public class FollowVisonTargetTheSecond extends Command {
 
 	// Called just before this Command runs the first time
 	@Override
-	protected void initialize() {}
+	protected void initialize() {
+		// SuperStructure.getElevator().getMaster().configPeakOutputForward(0);
+		// SuperStructure.getElevator().getMaster().configPeakOutputReverse(0);
+	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
@@ -62,8 +66,8 @@ public class FollowVisonTargetTheSecond extends Command {
 	public void Update_Limelight_Tracking() {
 		// These numbers must be tuned for your Robot!  Be careful!
 		final double STEER_K = 0.05;                    // how hard to turn toward the target
-		final double DRIVE_K = 0.26 * 1.6;                    // how hard to drive fwd toward the target
-		final double DESIRED_TARGET_AREA = 5;         // Area of the target when the robot reaches the wall
+		final double DRIVE_K = 0.26 * 1.6 * 1.6;                    // how hard to drive fwd toward the target
+		final double DESIRED_TARGET_AREA = this.targetArea;         // Area of the target when the robot reaches the wall
 		final double MAX_DRIVE = 0.7;                   // Simple speed limit so we don't drive too fast
 
 		double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
@@ -71,8 +75,11 @@ public class FollowVisonTargetTheSecond extends Command {
 		double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
 		double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
 
-		if (Math.abs(DESIRED_TARGET_AREA - ta) < 0.3)
-			m_isDone = true;
+		if (Math.abs(DESIRED_TARGET_AREA - ta) < 0.3){
+			System.out.println("WE DONE BOIS");
+			m_isDone = true;}
+
+		System.out.println("DESIRED_TARGET_AREA - ta: " + (DESIRED_TARGET_AREA - ta));
 
 		if (tv < 1.0) {
 			m_LimelightHasValidTarget = false;
