5b626d8c019b871fe0a90b3e6f6e83a0b3ff26f3
Matt <matthew.morley.ca@gmail.com>
2019-03-04 12:13:34 -0800

    literally an encoder

---
+++ src/main/java/frc/robot/subsystems/Sensors.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class Sensors extends Subsystem {
+  SPI kWristEncoder, kElbowEncoder;
+  final int encoderCPR = 2048;
+
+  public Sensors(SPI.Port wristPort, SPI.Port elbowPort) {
+    this.kWristEncoder = new SPI(wristPort);
+    this.kElbowEncoder = new SPI(elbowPort);
+  }
+
+
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
