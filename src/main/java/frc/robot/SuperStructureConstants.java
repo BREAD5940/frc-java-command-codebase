63d252cfe8694d5c552ae9a4275ea2b759c7dd9f
Matt <matthew.morley.ca@gmail.com>
2019-03-04 09:44:37 -0800

    cherry pick over constants file

--- src/main/java/frc/robot/SuperStructureConstants.java
+++ src/main/java/frc/robot/SuperStructureConstants.java
@@ -12,9 +12,7 @@ public class SuperStructureConstants {
 		public static final RoundRotation2d kWristMin = RoundRotation2d.getDegree(-45); // relative
 		public static final RoundRotation2d kWristMax = RoundRotation2d.getDegree(90); // relative
 		public static final Length intakeOut = LengthKt.getInch(19); //FIXME check
-		@Deprecated
 		public static final Length intakeDown = LengthKt.getInch(6); //FIXME check
-		@Deprecated
 		public static final Length intakeUp = LengthKt.getInch(12); //FIXME check
 	}
 
@@ -22,6 +20,8 @@ public class SuperStructureConstants {
 		public static final RoundRotation2d kElbowMin = RoundRotation2d.getDegree(-180); // absolute
 		public static final RoundRotation2d kElbowMax = RoundRotation2d.getDegree(15); // absolute
 		public static final Length carriageToIntake = LengthKt.getInch(12); //FIXME verify
+		public static final RoundRotation2d kStowedAngle = RoundRotation2d.getDegree(-80); //FIXME completely arb
+		public static final RoundRotation2d kClearFirstStageMinElbowAngle = RoundRotation2d.getDegree(-70); //FIXME
 
 	}
 
@@ -30,11 +30,12 @@ public class SuperStructureConstants {
 		public static final Length top = RobotConfig.elevator.elevator_maximum_height;
 		public static final Length crossbarBottom = LengthKt.getInch(35); //FIXME verify
 		public static final Length crossbarWidth = LengthKt.getInch(4); //FIXME verify
-		public static final Length longClimb = LengthKt.getInch(20); //FIXME actual number, perhaps?
+		public static final Length kElevatorLongRaiseDistance = LengthKt.getInch(27);//FIXME mostly arb.
+		public static final Length kClearFirstStageMaxHeight = crossbarBottom; //yay its redundant
+		public static final Length kElevatorApproachingThreshold = LengthKt.getInch(2); //FIXME
 
 	}
 
-	public static final Length electronicsHeight = LengthKt.getInch(0);
 	public static final Mass kHatchMass = MassKt.getLb(2.4); // FIXME check mass
 	public static final Mass kCargoMass = MassKt.getLb(1); // FIXME check mass
 }
