c4084f6462bfb5158d65038512817f0887f68599
Matt <matthew.morley.ca@gmail.com>
2019-03-09 21:59:33 -0800

    2 hatch minus superstructure moves

--- src/main/java/frc/robot/commands/auto/routines/TwoHatchOneCargo.java
+++ src/main/java/frc/robot/commands/auto/routines/TwoHatchOneCargo.java
@@ -52,95 +52,45 @@ public class TwoHatchOneCargo extends VisionCommandGroup {
 		/* Get a trajectory to move to the cargo ship. THE ROBOT IS REVERSED */
 		TimedTrajectory<Pose2dWithCurvature> traject = Trajectories.generatedLGTrajectories.get("habR" + " to " + "rocketRF"); //current trajectory from hashmap in Trajectories
 
-		addParallel(new SuperstructureGoToState(iPosition.HATCH_SLAM_ROCKET_INSIDE_PREP)); // move arm inside to prep state
+		// addParallel(new SuperstructureGoToState(iPosition.HATCH_SLAM_ROCKET_INSIDE_PREP)); // move arm inside to prep state
 		addParallel(new LimeLight.SetLEDs(LimeLight.LEDMode.kON));
 		addParallel(new LimeLight.setPipeline(PipelinePreset.k3dVision));
 		addSequential(DriveTrain.getInstance().followTrajectoryWithGear(traject, TrajectoryTrackerMode.RAMSETE, Gear.LOW, true)); //drive to goal
 
-		addParallel(new SuperstructureGoToState(fieldPositions.hatchMiddleGoal, iPosition.HATCH));
-		// addSequential(new SplineToVisionTarget(/*this.getPoseStorage1(), */LengthKt.getInch(0), LengthKt.getInch(30), 6.5));
-		// addSequential(new FollowVisionTargetTheSecond(4.3));
-		addSequential(new DriveDistanceToVisionTarget(LengthKt.getInch(35), 5));
+		// addParallel(new SuperstructureGoToState(fieldPositions.hatchMiddleGoal, iPosition.HATCH));
+		addSequential(new FollowVisionTargetTheSecond(4.3));
 
 		addSequential(new RunIntake(-1, 0, 1));
 
 		// back up 3 feet
-		addParallel(new SuperstructureGoToState(iPosition.HATCH_GRAB_INSIDE_PREP));
+		// addParallel(new SuperstructureGoToState(iPosition.HATCH_GRAB_INSIDE_PREP));
 		addSequential(new DriveDistanceTheSecond(LengthKt.getFeet(3), true));
 
 		// spline over to the rocket
-		// var rocketToLoading = Trajectories.generatedLGTrajectories.get("rocketRF to loadingR");
-		// addSequential(DriveTrain.getInstance().followTrajectoryWithGear(traject, TrajectoryTrackerMode.RAMSETE, Gear.HIGH, true)); //drive to goal
+		var rocketToLoading = Trajectories.generatedLGTrajectories.get("rocketRF to loadingR");
+		addSequential(DriveTrain.getInstance().followTrajectoryWithGear(traject, TrajectoryTrackerMode.RAMSETE, Gear.LOW, true)); //drive to goal
 
-		addSequential(new PickupHatch());
-
-		// var loadingToRocketClose = Trajectories.generatedLGTrajectories.get("loadingR to rocketRC");
-		// addSequential(DriveTrain.getInstance().followTrajectoryWithGear(traject, TrajectoryTrackerMode.RAMSETE, Gear.HIGH, true)); //drive to goal
-		addSequential(new LimeLight.SetLEDs(LimeLight.LEDMode.kON));
-
-		addParallel(new SuperstructureGoToState(fieldPositions.hatchMiddleGoal, iPosition.HATCH));
-
+		
+		// addParallel(new SuperstructureGoToState(fieldPositions.hatchMiddleGoal, iPosition.HATCH));
+		
 		addSequential(new FollowVisionTargetTheSecond(5));
-
+		
 		addSequential(new RunIntake(-1, 0, 1));
+		
+		// addParallel(new LimeLight.SetLEDs(LimeLight.LEDMode.kOFF));
+		var loadingToRocketFar = Trajectories.generatedLGTrajectories.get("loadingR to rocketRF");
+		addSequential(DriveTrain.getInstance().followTrajectoryWithGear(traject, TrajectoryTrackerMode.RAMSETE, Gear.LOW, true)); //drive to goal
+		addSequential(new FollowVisionTargetTheSecond(4.3));
+		
+		
+		
 
-		addParallel(new LimeLight.SetLEDs(LimeLight.LEDMode.kOFF));
-		/* Move from middle of cargo ship to loading station on the same side to pick up a hatch */
-		// cStart = "cargoM" + side;
-		// cPiece = HeldPiece.NONE;
-
-		// traject = Trajectories.generatedHGTrajectories.get("rocketLF" + " to " + "loading" + side); //current trajectory from hashmap in Trajectorie
-		// this.addParallel(new SuperstructureGoToState(iPosition.HATCH_GRAB_INSIDE_PREP));
-		// this.addSequential(DriveTrain.getInstance().followTrajectory(Trajectories.reverse3FeetLowGear, TrajectoryTrackerMode.RAMSETE, false)); //back up mega fast to clear rocket
-		// this.addSequential(DriveTrain.getInstance().followTrajectory(traject, TrajectoryTrackerMode.RAMSETE, false)); //drive to goal
-		// addSequential(new SetTempPoseFromVisionTarget(this, PoseStorage.POSE1, false));
-		// addSequential(new SplineToVisionTarget(this.getPoseStorage1(), LengthKt.getInch(0), LengthKt.getInch(30), 6.5));
-
-		// /* Go right up to the cargo ship from the loading station */
-		// cStart = "loading" + side;
-		// cPiece = HeldPiece.HATCH;
-
-		// traject = Trajectories.generatedHGTrajectories.get(cStart + " to " + "cargo" + side + '1'); //current trajectory from hashmap in Trajectorie
-		// motion = new AutoMotion(GoalHeight.LOW, GoalType.CARGO_HATCH, false);
-		// trajects.add(traject);
-		// motions.add(motion);
-		// this.addParallel(motion.getPrepCommand());
-		// this.addSequential(DriveTrain.getInstance().followTrajectory(traject, TrajectoryTrackerMode.RAMSETE, false)); //drive to goal
-
-		// // turn 90 degrees to face the goal
-		// this.addSequential(new TurnInPlace(Trajectories.locations.get("cargo" + side + '1').component2(), true)); // TODO check the angle math here! 
-		// this.addSequential(motion.getBigCommandGroup()); //move the intake for hatch placement
-
-		// /* Go from cargo side 1 to the depot */
-		// cStart = "cargo" + side + '1';
-		// cPiece = HeldPiece.NONE;
-
-		// traject = Trajectories.generatedHGTrajectories.get(cStart + " to " + "depot" + side); //current trajectory from hashmap in Trajectorie
-		// motion = new AutoMotion(GoalHeight.LOW, GoalType.RETRIEVE_CARGO, false);
-		// trajects.add(traject);
-		// motions.add(motion);
-		// this.addParallel(motion.getPrepCommand());
-		// this.addSequential(DriveTrain.getInstance().followTrajectory(traject, TrajectoryTrackerMode.RAMSETE, false)); //drive to goal
-		// this.addSequential(motion.getBigCommandGroup());
-		// // this.addSequential(new DelayCommand(TimeUnitsKt.getSecond(0.5)).getWrappedValue());
-
-		// /* Go from depot to cargo ship ~~2~~ 1 darnit you're right. Thanks 10pm me */
-		// cStart = "depot" + side;
-		// cPiece = HeldPiece.CARGO;
-		// traject = Trajectories.generatedHGTrajectories.get(cStart + " to " + "cargo" + side + '1'); //current trajectory from hashmap in Trajectorie
-		// motion = new AutoMotion(GoalHeight.OVER, GoalType.CARGO_CARGO, false);
-		// trajects.add(traject);
-		// motions.add(motion);
-		// this.addParallel(motion.getPrepCommand());
-		// this.addSequential(DriveTrain.getInstance().followTrajectory(traject, TrajectoryTrackerMode.RAMSETE, false)); //drive to goal
-		// this.addSequential(new TurnInPlace(Rotation2dKt.getDegree(90), true)); // TODO check the angle
-		// //FIXME this would have to raise the elevator
-		// this.addSequential(motion.getBigCommandGroup()); //deposit cargo
-
+		addSequential(new LimeLight.SetLEDs(LimeLight.LEDMode.kOFF));
+		
 	}
-
+	
 	// id functions
-
+	
 	/**
 	 * identification function
 	 * @return
