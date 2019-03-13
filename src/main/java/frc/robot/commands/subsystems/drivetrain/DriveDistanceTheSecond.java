e347fac784c4808bdbf48a15067237054276f04e
Matt <matthew.morley.ca@gmail.com>
2019-03-13 16:55:24 -0700

    fixed non resetting drive distance

--- src/main/java/frc/robot/commands/subsystems/drivetrain/DriveDistanceTheSecond.java
+++ src/main/java/frc/robot/commands/subsystems/drivetrain/DriveDistanceTheSecond.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d;
+import org.ghrobotics.lib.mathematics.twodim.geometry.Translation2d;
 import org.ghrobotics.lib.mathematics.units.Length;
 import org.ghrobotics.lib.mathematics.units.LengthKt;
 import org.ghrobotics.lib.mathematics.units.Rotation2dKt;
@@ -14,6 +15,7 @@ import org.ghrobotics.lib.mathematics.units.derivedunits.VelocityKt;
 import org.ghrobotics.lib.mathematics.units.nativeunits.NativeUnit;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.commands.auto.Trajectories;
 import frc.robot.lib.motion.Util;
 import frc.robot.subsystems.DriveTrain;
@@ -21,8 +23,8 @@ import frc.robot.subsystems.DriveTrain.TrajectoryTrackerMode;
 
 public class DriveDistanceTheSecond extends Command {
 
-	private static final Velocity<Length> kCruiseVel = VelocityKt.getVelocity(LengthKt.getFeet(4));
-	private static final Acceleration<Length> kDefaultAcceleration = AccelerationKt.getAcceleration(LengthKt.getFeet(8));
+	private static Velocity<Length> kCruiseVel = VelocityKt.getVelocity(LengthKt.getFeet(4));
+	private static Acceleration<Length> kDefaultAcceleration = AccelerationKt.getAcceleration(LengthKt.getFeet(8));
 	// private final DriveTrain drive;
 	private Velocity<NativeUnit> kCruiseVelTicks;
 	private Acceleration<NativeUnit> kAccelTicks;
@@ -42,48 +44,80 @@ public class DriveDistanceTheSecond extends Command {
 	}
 
 	public DriveDistanceTheSecond(Length distance, Velocity<Length> vel, boolean reversed) {
+		// this.distance = distance;
+		// this.vel = vel;
+		// this.reversed = reversed;
 		this.distance = distance;
 		this.vel = vel;
 		this.reversed = reversed;
+		requires(DriveTrain.getInstance());
 	}
 
-	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
-		List<Pose2d> waypoints = (!reversed) ? Arrays.asList(
-				new Pose2d(LengthKt.getInch(0), LengthKt.getInch(30), Rotation2dKt.getDegree(0)),
-				new Pose2d(distance, LengthKt.getInch(30), Rotation2dKt.getDegree(0)))
-				: Arrays.asList(
-						new Pose2d(LengthKt.getInch(0), LengthKt.getInch(30), Rotation2dKt.getDegree(0)),
-						new Pose2d(distance.times(-1), LengthKt.getInch(30), Rotation2dKt.getDegree(0)));
+		
 
-		var trajectory = Trajectories.generateTrajectoryLowGear(waypoints, reversed);
+		var currentPose = DriveTrain.getInstance().getLocalization().getRobotPosition();
+		System.out.println("CURRENT POSE: " + currentPose.getTranslation().getX().getInch() + "," + currentPose.getTranslation().getY().getInch());
+		Translation2d offsetTrans;
+		if(!reversed) {
+			offsetTrans = new Translation2d(distance, currentPose.getRotation());
+		} else {
+			offsetTrans = new Translation2d(distance.times(-1), currentPose.getRotation());
+		}
+		var offsetPose = currentPose.plus(new Pose2d(offsetTrans, Rotation2dKt.getDegree(0)));
 
-		System.out.println("FIRST POSE: " + Util.toString(trajectory.getFirstState().getState().getPose()) + " LAST POSE: " + Util.toString(trajectory.getLastState().getState().getPose()));
+		System.out.println("OFFSET POSE: " + offsetPose.getTranslation().getX().getInch() + "," + offsetPose.getTranslation().getY().getInch());
 
-		mCommand = DriveTrain.getInstance().followTrajectory(trajectory, TrajectoryTrackerMode.RAMSETE, true);
 
-		clearRequirements();
-		mCommand.start();
-		commandStarted = true;
+		var waypoints = Arrays.asList(
+			currentPose, offsetPose
+		);
+
+		// List<Pose2d> waypoints = (!reversed) ? Arrays.asList(
+		// 	new Pose2d(LengthKt.getInch(0), LengthKt.getInch(30), Rotation2dKt.getDegree(0)),
+		// 	new Pose2d(distance, LengthKt.getInch(30), Rotation2dKt.getDegree(0)))
+		// 	: Arrays.asList(
+		// 			new Pose2d(LengthKt.getInch(0), LengthKt.getInch(30), Rotation2dKt.getDegree(0)),
+		// 			new Pose2d(distance.times(-1), LengthKt.getInch(30), Rotation2dKt.getDegree(0)));
+
+	var trajectory = Trajectories.generateTrajectoryLowGear(waypoints, reversed);
+
+	System.out.println("FIRST POSE: " + Util.toString(trajectory.getFirstState().getState().getPose()) + " LAST POSE: " + Util.toString(trajectory.getLastState().getState().getPose()));
+
+	mCommand = DriveTrain.getInstance().followTrajectory(trajectory, TrajectoryTrackerMode.RAMSETE, false);
+
+	// addSequential(mCommand);
+	clearRequirements();
+	mCommand.start();
+
+	// mCommand.start();
+	commandStarted = true;
+
 	}
 
-	// Called repeatedly when this Command is scheduled to run
-	@Override
-	protected void execute() {}
+// 	// Called just before this Command runs the first time
+// 	@Override
+// 	protected void initialize() {
+	
+// 	}
+
+// 	// Called repeatedly when this Command is scheduled to run
+// 	@Override
+// 	protected void execute() {}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return mCommand.isCompleted() && commandStarted;
+		return  mCommand.isCompleted() && commandStarted;
 	}
 
-	// Called once after isFinished returns true
-	@Override
-	protected void end() {}
+// 	// Called once after isFinished returns true
+// 	@Override
+// 	protected void end() {}
 
-	// Called when another command which requires one or more of the same
-	// subsystems is scheduled to run
-	@Override
-	protected void interrupted() {}
+// 	// Called when another command which requires one or more of the same
+// 	// subsystems is scheduled to run
+// 	@Override
+// 	protected void interrupted() {}
 }
