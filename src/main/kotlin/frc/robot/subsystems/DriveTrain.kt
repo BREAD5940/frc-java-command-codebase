26ca1b70b0cf0025da9c060684d5edcfce6c110a
Matt <matthew.morley.ca@gmail.com>
2019-03-13 15:53:22 -0700

    K otlin gang

--- src/main/kotlin/frc/robot/subsystems/DriveTrain.kt
+++ src/main/kotlin/frc/robot/subsystems/DriveTrain.kt
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.command.Subsystem
 /**
  * Add your docs here.
  */
-class ExampleSubsystem: Subsystem() {
+class DriveTrain: Subsystem() {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
       
