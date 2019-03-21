a40cc08942cbded57a4736b310035c07b5438dfc
Matt <matthew.morley.ca@gmail.com>
2019-03-21 16:49:26 -0700

    Update PlaceHatch.java

--- src/main/java/frc/robot/commands/auto/routines/PlaceHatch.java
+++ src/main/java/frc/robot/commands/auto/routines/PlaceHatch.java
@@ -1,53 +1,53 @@
-package frc.robot.commands.auto.routines;
+// package frc.robot.commands.auto.routines;
 
-import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d;
-import org.ghrobotics.lib.mathematics.units.LengthKt;
+// import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d;
+// import org.ghrobotics.lib.mathematics.units.LengthKt;
 
-import edu.wpi.first.wpilibj.command.PrintCommand;
-import frc.robot.commands.auto.groups.VisionCommandGroup;
-import frc.robot.commands.subsystems.drivetrain.DrivePower;
-import frc.robot.commands.subsystems.drivetrain.SplineToVisionTarget;
-import frc.robot.commands.subsystems.superstructure.RunIntake;
-import frc.robot.commands.subsystems.superstructure.SuperstructureGoToState;
-import frc.robot.subsystems.superstructure.SuperStructure.iPosition;
+// import edu.wpi.first.wpilibj.command.PrintCommand;
+// import frc.robot.commands.auto.groups.VisionCommandGroup;
+// import frc.robot.commands.subsystems.drivetrain.DrivePower;
+// import frc.robot.commands.subsystems.drivetrain.SplineToVisionTarget;
+// import frc.robot.commands.subsystems.superstructure.RunIntake;
+// import frc.robot.commands.subsystems.superstructure.SuperstructureGoToState;
+// import frc.robot.subsystems.superstructure.SuperStructure.iPosition;
 
-public class PlaceHatch extends VisionCommandGroup {
+// public class PlaceHatch extends VisionCommandGroup {
 
-	// Pose2d mMeasuredPose = new Pose2d();
+// 	// Pose2d mMeasuredPose = new Pose2d();
 
-	// public void setPose(Pose2d newPose) {
-	// 	System.out.println("============================ Calleback called! ============================");
-	// 	System.out.println("New pose: " + mMeasuredPose.getRotation().getDegree());
-	// 	this.mMeasuredPose = newPose;
-	// }
+// 	// public void setPose(Pose2d newPose) {
+// 	// 	System.out.println("============================ Calleback called! ============================");
+// 	// 	System.out.println("New pose: " + mMeasuredPose.getRotation().getDegree());
+// 	// 	this.mMeasuredPose = newPose;
+// 	// }
 
-	public PlaceHatch() {
-		this(new Pose2d(), false);
-	}
+// 	public PlaceHatch() {
+// 		this(new Pose2d(), false);
+// 	}
 
-	/**
-	 * Place a hatch using a vision target tracker followed by a spline target. No Splines, yet. TODO splines for this boi
-	 * 
-	 * @author Matthew Morley
-	 */
-	public PlaceHatch(Pose2d goalPose, boolean setOdometry) {
+// 	/**
+// 	 * Place a hatch using a vision target tracker followed by a spline target. No Splines, yet. TODO splines for this boi
+// 	 * 
+// 	 * @author Matthew Morley
+// 	 */
+// 	public PlaceHatch(Pose2d goalPose, boolean setOdometry) {
 
-		addSequential(new PrintCommand("Placing a hatch;;;...."));
+// 		addSequential(new PrintCommand("Placing a hatch;;;...."));
 
-		addParallel(new SuperstructureGoToState(iPosition.HATCH_SLAM_ROCKET_INSIDE.elevator.plus(iPosition.kOffsetFromL1toL2), iPosition.HATCH_SLAM_ROCKET_INSIDE.jointAngles));
-		addSequential(new SplineToVisionTarget(/*this.getPoseStorage1(), */LengthKt.getInch(0), LengthKt.getInch(30), 6.5));
-		addSequential(new SuperstructureGoToState(iPosition.HATCH_SLAM_ROCKET_INSIDE), 1);
-		addSequential(new RunIntake(-1, 0, 1));
+// 		addParallel(new SuperstructureGoToState(iPosition.HATCH_SLAM_ROCKET_INSIDE.elevator.plus(iPosition.kOffsetFromL1toL2), iPosition.HATCH_SLAM_ROCKET_INSIDE.jointAngles));
+// 		addSequential(new SplineToVisionTarget(/*this.getPoseStorage1(), */LengthKt.getInch(0), LengthKt.getInch(30), 6.5));
+// 		addSequential(new SuperstructureGoToState(iPosition.HATCH_SLAM_ROCKET_INSIDE), 1);
+// 		addSequential(new RunIntake(-1, 0, 1));
 
-		addParallel(new RunIntake(-1, 0, 0.75));
-		addSequential(new DrivePower(-0.2, 0.75));
+// 		addParallel(new RunIntake(-1, 0, 0.75));
+// 		addSequential(new DrivePower(-0.2, 0.75));
 
-		addSequential(new PrintCommand("We done bois, hatch placed...."));
+// 		addSequential(new PrintCommand("We done bois, hatch placed...."));
 
-		// addParallel(new SuperstructureGoToState(new ElevatorState(fieldPositions.hatchLowGoal.plus(LengthKt.getInch(3))), iPosition.HATCH_PITCHED_UP));
+// 		// addParallel(new SuperstructureGoToState(new ElevatorState(fieldPositions.hatchLowGoal.plus(LengthKt.getInch(3))), iPosition.HATCH_PITCHED_UP));
 
-		// addSequential(new PrintCommand("driving at a power in reverse"));
+// 		// addSequential(new PrintCommand("driving at a power in reverse"));
 
-		// addSequential(new DriveDistanceTheSecond(LengthKt.getFeet(.5), true)); // TODO run the next spline, saves time, vs backing up
-	}
-}
+// 		// addSequential(new DriveDistanceTheSecond(LengthKt.getFeet(.5), true)); // TODO run the next spline, saves time, vs backing up
+// 	}
+// }
