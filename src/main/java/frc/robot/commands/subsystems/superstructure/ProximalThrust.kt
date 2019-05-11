2bf0704b512554feb057a0cf14569fb38584222c
Matthew Morley <matthew.morley.ca@gmail.com>
2019-05-11 15:07:20 -0700

    Update ProximalThrust.kt

--- src/main/java/frc/robot/commands/subsystems/superstructure/ProximalThrust.kt
+++ src/main/java/frc/robot/commands/subsystems/superstructure/ProximalThrust.kt
@@ -1,15 +1,28 @@
 package frc.robot.commands.subsystems.superstructure
 
 import edu.wpi.first.wpilibj.command.Command
+import frc.robot.lib.obj.RoundRotation2d
+import frc.robot.states.ElevatorState
+import frc.robot.states.SuperStructureState
 import frc.robot.subsystems.superstructure.Elevator
 import frc.robot.subsystems.superstructure.SuperStructure
 import org.ghrobotics.lib.mathematics.twodim.geometry.Pose2d
+import org.ghrobotics.lib.mathematics.twodim.geometry.Translation2d
+import org.ghrobotics.lib.mathematics.units.inch
+import org.ghrobotics.lib.mathematics.units.meter
+import frc.robot.subsystems.superstructure.SuperStructure.getDumbWrist
+import org.ghrobotics.lib.mathematics.units.radian
+
+val Number.roundRadian get() = RoundRotation2d.getRadian(toDouble())
+@Suppress("unused")
+val Number.roundDegree get() = RoundRotation2d.getDegree(toDouble())
 
 class ProximalThrust(
         private val structure: SuperStructure,
         private val elevator : Elevator,
         private val pokeDistance: Double,
-        private val proximalLen: Double
+        private val proximalLen: Double,
+        private val wristAngle: Double
 ) : Command() {
 
     init {
@@ -19,14 +32,50 @@ class ProximalThrust(
         requires(elevator)
     }
 
-    lateinit var startLocation: Pose2d
+    var startPose: Translation2d? = null
+    var startState: SuperStructureState? = null
+    var goalPose: Translation2d? = null
+    var lastState = SuperStructureState()
+    var lastProximalAngle = 0.0
+    var iteratorFinished = false
+
+    companion object {
+        val kProximalLen = 17.inch.meter
+        val kProximalSpeed = Math.toRadians(50.0)
+    }
 
     override fun initialize() {
+        startState = structure.currentState
+        startPose = SuperStructure.getCurrentStateAsPose(startState)
+        goalPose = startPose!! + Translation2d(pokeDistance, 0.0)
+        lastState = startState!!
+    }
+
+    override fun execute() {
+        val currentState = structure.currentState
+
+        val newWantedProximal = lastState.elbowAngle.radian + kProximalSpeed * 0.02
+
+        val startProximalTranslation2d = Translation2d.fromRotation2d(startState!!.elbowAngle.radian, kProximalLen)
+
+        val newProximalTranslation2d = Translation2d.fromRotation2d(newWantedProximal, kProximalLen)
+
+        if (newProximalTranslation2d.x > pokeDistance) {iteratorFinished = true ; return}
+
+        val deltaHeight = newProximalTranslation2d.y - startProximalTranslation2d.y
+
+        val newWantedElevator = startState!!.elevatorHeight + deltaHeight.meter
+
+        val newWantedWrsit = getDumbWrist(0.roundRadian, currentState.elbowAngle)
+
+        structure.elbow.requestAngle(newWantedProximal.roundRadian)
+        structure.wrist.requestAngle(newWantedWrsit)
+        elevator.setPositionSetpoint(ElevatorState(newWantedElevator))
 
     }
 
     override fun isFinished(): Boolean {
-        return false
+        return iteratorFinished
     }
 
 }
